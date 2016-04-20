f62d31fd745080be9f5d33311e232549a3ba3a64
Jonathan Younger <jonathan@daikini.com>
2016-04-20 11:16:47 -0600

    Android WIP

--- Libraries/Emdros/android/src/main/jni/Application.mk
+++ Libraries/Emdros/android/src/main/jni/Application.mk
@@ -1,16 +1,4 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libEmdros
-LOCAL_C_INCLUDES := ../../../../../../../Libraries/Emdros/src/
-LOCAL_SRC_FILES := \
-        ../../../../../../../Libraries/Emdros/src/emdros_c_amalgamation.c \
-        ../../../../../../../Libraries/Emdros/src/emdros_amalgamation.cpp \
-        ../../../../../../../Libraries/Emdros/src/bucket.cpp \
-        ../../../../../../../Libraries/Emdros/src/harvest.cpp
-
-LOCAL_CPPFLAGS := -DUSE_SYSTEM_SQLITE3=0  -DTHREADSAFE=1
-LOCAL_CFLAGS := -DTHREADSAFE=1
-LOCAL_EXPORT_LDLIBS := -lz
-LOCAL_CPP_FEATURES += exceptions
-include $(BUILD_STATIC_LIBRARY)
+APP_MODULES := Emdros
+APP_STL := stlport_shared
+APP_PLATFORM := android-16
+APP_ABI := armeabi-v7a x86
