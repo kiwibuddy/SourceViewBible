175497c63503fc67bc07c7f285d24b49f699a3c2
Jonathan Younger <jonathan@daikini.com>
2016-04-21 09:05:11 -0600

    Android WIP

---
+++ Libraries/Emdros/android/src/main/java/com/sourceviewbible/emdros/Emdros.java
@@ -0,0 +1,15 @@
+package com.sourceviewbible.emdros;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Emdros {
+  private long emdrosEnv;
+  private Map<String, String> options;
+
+  public Emdros(Map<String,String> options) {
+    this.options = options;
+  }
+
+  public native void connect();
+}
