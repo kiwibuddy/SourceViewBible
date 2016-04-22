eb9c139de16bf88c12e2775662cdd4210b266310
Jonathan Younger <jonathan@daikini.com>
2016-04-22 09:36:51 -0600

    [Android] Copy database onCreate

--- TechnicalValidation/HarvestEmdros/android/app/src/main/java/com/harvestemdros/MainActivity.java
+++ TechnicalValidation/HarvestEmdros/android/app/src/main/java/com/harvestemdros/MainActivity.java
@@ -28,39 +28,43 @@ import android.os.Environment;
 import com.sourceviewbible.emdros.*; // import the Emdros package
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AssetManager assetManager = getAssets();
+        try {
+            String outfilename = getFilesDir().toString() + File.separator + "sourceview.bpt";
+            Log.v("Emdros", "Internal storage: " + outfilename);
+
+            String[] files = assetManager.list("");
+            for (String filename : files) {
+              Log.v("Emdros", "Asset File: " + filename);
+            }
+
+            InputStream myinput = assetManager.open("sourceview/datasets/sourceview.bpt");
+            OutputStream myoutput = new FileOutputStream(outfilename);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = myinput.read(buffer))>0) {
+                myoutput.write(buffer,0,length);
+            }
+            myoutput.flush();
+            myoutput.close();
+            myinput.close();
+
+            Log.v("Emdros", "Copied: " + outfilename);
+        } catch (IOException e) {
+             Log.v("Emdros", "uh oh: " + e.toString());
+        }
+    }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
-        AssetManager assetManager = getAssets();
-        try {
-          String outfilename = getFilesDir().toString() + File.separator + "sourceview.bpt";
-          Log.v("Emdros", "Internal storage: " + outfilename);
-
-          String[] files = assetManager.list("");
-          for (String filename : files) {
-            Log.v("Emdros", "Asset File: " + filename);
-          }
-
-          InputStream myinput = assetManager.open("sourceview/datasets/sourceview.bpt");
-          OutputStream myoutput = new FileOutputStream(outfilename);
-          byte[] buffer = new byte[1024];
-          int length;
-          while ((length = myinput.read(buffer))>0) {
-              myoutput.write(buffer,0,length);
-          }
-          myoutput.flush();
-          myoutput.close();
-          myinput.close();
-
-          Log.v("Emdros", "Copied: " + outfilename);
-
-        } catch (IOException e) {
-             Log.v("Emdros", "uh oh: " + e.toString());
-        }
-
         return "HarvestEmdros";
     }
 
