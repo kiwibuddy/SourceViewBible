4bb3235d90dd78a5851e85465bf16cfb509e166e
Jonathan Younger <jonathan@daikini.com>
2016-05-10 10:37:49 -0600

    Android Segmented Control WIP

--- App/app/components/common/segmented-control.android.js
+++ App/app/components/common/segmented-control.android.js
@@ -14,20 +14,42 @@ export default class SegmentedControl extends Component {
     selectedIndex: PropTypes.number
   };
 
+  state: {
+    __indicatorLeft: any,
+    __indicatorWidth: any
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      __indicatorLeft: new Animated.Value(0),
+      __indicatorWidth: new Animated.Value(0)
+    };
+  }
+
   render() {
     const values = this.props.values;
     let index = 0;
     const buttons = values.map((value) => {
-      let isButtonSelected = index === this.props.selectedIndex;
+      let buttonIndex = index;
+      let isButtonSelected = buttonIndex === this.props.selectedIndex;
       let tintColor = (isButtonSelected ? this.props.tintColor : null);
       index++;
-      return this._renderButton(value, tintColor);
+      return this._renderButton(buttonIndex, value, tintColor);
     });
 
-    const indicatorStyle = [styles.indicator, {backgroundColor: this.props.tintColor}];
+    const indicatorStyle = [styles.indicator, {
+      backgroundColor: this.props.tintColor,
+      left: this.state.__indicatorLeft,
+      width: this.state.__indicatorWidth
+    }];
 
     return (
-      <View style={[styles.tabBar, this.props.style]}>
+      <View
+        style={[styles.tabBar, this.props.style]}
+        onLayout={this._measureSelectedButton.bind(this)}
+      >
         <View style={styles.buttons}>
           {buttons}
         </View>
@@ -36,12 +58,19 @@ export default class SegmentedControl extends Component {
     );
   }
 
-  _renderButton(title: String, tintColor: ColorPropType) {
+  _renderButton(index: PropTypes.number, title: String, tintColor: ColorPropType) {
     return(
-      <TouchableOpacity key={'button-' + title} style={styles.button} onPress={this.props.onButtonPress}>
+      <TouchableOpacity key={'button-' + title} ref={index} style={styles.button} onPress={this.props.onButtonPress}>
         <Text style={[styles.buttonTitle, {color: tintColor}]}>{title.toLocaleUpperCase()}</Text>
       </TouchableOpacity>
     );
+  }
+
+  _measureSelectedButton() {
+    this.refs[this.props.selectedIndex].measure((ox, oy, width, height, px, py) => {
+      this.state.__indicatorLeft.setValue(px);
+      this.state.__indicatorWidth.setValue(width);
+    });
   }
 }
 
