b55f7406a04d02e74ccc838c22ec658c0de1efc7
Jonathan Younger <jonathan@daikini.com>
2016-05-16 11:55:34 -0600

    Sphere color placeholders

--- App/app/Common/Colors.js
+++ App/app/Common/Colors.js
@@ -12,12 +12,12 @@ module.exports = {
   },
 
   spheres: {
-    family: '',
-    economics: '',
-    government: '',
-    religion: '',
-    education: '',
-    communication: '',
-    celebration: ''
+    family: '#FF3A2E',
+    economics: '#FF9700',
+    government: '#FECB01',
+    religion: '#4ED962',
+    education: '#25B8D7',
+    communication: '#5A5AD3',
+    celebration: '#E058AD'
   }
 };
