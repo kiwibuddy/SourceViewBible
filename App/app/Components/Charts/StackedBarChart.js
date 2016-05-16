f393ed8db544555d93a84add8679b77eebed10b2
Jonathan Younger <jonathan@daikini.com>
2016-05-16 09:42:13 -0600

    Use stateless component for charts

--- App/app/Components/Charts/StackedBarChart.js
+++ App/app/Components/Charts/StackedBarChart.js
@@ -8,41 +8,39 @@ import { BarChart, Bar } from './BarChart';
 import StyleSheet from '../../Common/StyleSheet';
 import Colors from '../../Common/Colors';
 
-export default class StackedBarChart extends Component {
-  static defaultProps = {
-    horizontal: false
-  };
+const StackedBarChart = (props: Object) => {
+  const style = [styles.container, {flexDirection: props.horizontal ? 'row' : 'column'}, props.style];
 
-  static propTypes = {
-    style: PropTypes.any,
-    data: PropTypes.any.isRequired,
-    horizontal: PropTypes.bool
-  };
+  return (
+    <BarChart style={style}>
+      <Bar
+        style={{backgroundColor: Colors.sources.black}}
+        dataKey={'black'}
+      />
+      <Bar
+        style={{backgroundColor: Colors.sources.red}}
+        dataKey={'red'}
+      />
+      <Bar
+        style={{backgroundColor: Colors.sources.green}}
+        dataKey={'green'}
+      />
+      <Bar
+        style={{backgroundColor: Colors.sources.blue}}
+        dataKey={'blue'}
+      />
+    </BarChart>
+  );
+}
 
-  render() {
-    const style = [styles.container, {flexDirection: this.props.horizontal ? 'row' : 'column'}, this.props.style];
+StackedBarChart.propTypes = {
+  style: PropTypes.any,
+  data: PropTypes.any.isRequired,
+  horizontal: PropTypes.bool
+};
 
-    return (
-      <BarChart style={style} data={this.props.data}>
-        <Bar
-          style={{backgroundColor: Colors.sources.black}}
-          dataKey={'black'}
-        />
-        <Bar
-          style={{backgroundColor: Colors.sources.red}}
-          dataKey={'red'}
-        />
-        <Bar
-          style={{backgroundColor: Colors.sources.green}}
-          dataKey={'green'}
-        />
-        <Bar
-          style={{backgroundColor: Colors.sources.blue}}
-          dataKey={'blue'}
-        />
-      </BarChart>
-    )
-  }
+StackedBarChart.defaultProps = {
+  horizontal: false
 }
 
 const styles = StyleSheet.create({
@@ -50,3 +48,5 @@ const styles = StyleSheet.create({
     flex: 1
   }
 });
+
+export default StackedBarChart;
