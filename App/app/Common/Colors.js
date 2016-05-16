b489521509c493f0719845aa7e30f891911e7e68
Jonathan Younger <jonathan@daikini.com>
2016-05-16 11:46:32 -0600

    Sphere Color Placeholders

--- App/app/Common/Colors.js
+++ App/app/Common/Colors.js
@@ -3,11 +3,21 @@
 
 module.exports = {
   tintColor: '#cf1e00',
-  
+
   sources: {
     black: '#333333',
     red: '#fc3d39',
     green: '#4cda65',
     blue: '#218aff',
+  },
+
+  spheres: {
+    family: '',
+    economics: '',
+    government: '',
+    religion: '',
+    education: '',
+    communication: '',
+    celebration: ''
   }
 };
