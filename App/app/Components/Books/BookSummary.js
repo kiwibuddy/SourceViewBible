cc60d2f1efe2057a86e4c5d3d36151337635264c
Jonathan Younger <jonathan@daikini.com>
2016-05-25 13:52:48 -0600

    Word Cloud WIP

--- App/app/Components/Books/BookSummary.js
+++ App/app/Components/Books/BookSummary.js
@@ -41,16 +41,16 @@ class BookSummary extends Component {
           backgroundColors={Colors.spheres[book.principalSphere].gradient}
           style={styles.wordCloud}
         >
-          <Text style={{backgroundColor:'transparent'}}>{words[0].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[1].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[2].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[3].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[4].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[5].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[6].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[7].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[8].word}</Text>
-          <Text style={{backgroundColor:'transparent'}}>{words[9].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[0].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[1].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[2].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[3].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[4].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[5].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[6].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[7].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[8].word}</Text>
+          <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[9].word}</Text>
         </WordCloud>
 
         <View style={styles.statisticsContainer}>
