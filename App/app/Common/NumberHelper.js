bde49f152a99b3ae68665ad8fade72f4b83eb904
Jonathan Younger <jonathan@daikini.com>
2016-05-24 18:11:08 -0600

    Adjust words per minute reading time

--- App/app/Common/NumberHelper.js
+++ App/app/Common/NumberHelper.js
@@ -4,8 +4,10 @@
 const moment = require('moment');
 require('moment-duration-format');
 
+const WORDS_PER_MINUTE = 150;
+
 function ReadingTime(words: number) {
-  const minutes = words / 200;
+  const minutes = words / WORDS_PER_MINUTE;
   const time = minutes * 60 * 1000;
   const displayed = Math.ceil(minutes);
   return moment.duration(displayed, "minutes").format("h [hr] m [min]");
