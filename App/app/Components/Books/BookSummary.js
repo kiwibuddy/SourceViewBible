449555a48163a79a1ba316bbc3d047108e2bf975
Harold <harold@overcommittedapps.com>
2016-05-25 14:58:45 -0700

    Word clouds!

--- App/app/Components/Books/BookSummary.js
+++ App/app/Components/Books/BookSummary.js
@@ -41,26 +41,28 @@ class BookSummary extends Component {
           backgroundColors={Colors.spheres[book.principalSphere].gradient}
           style={styles.wordCloud}
         >
-          <View style={{opacity: 0.75}}>
-            <Text style={[styles.wc1, {alignSelf: 'center', backgroundColor:'transparent'}]}>{words[0].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[1].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[2].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[3].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[4].word}</Text>
+          <View style={{opacity: 0.8}}>
+            <Text style={[styles.wc1, {top: 50, alignSelf: 'center'}]}>{words[0].word}</Text>
           </View>
-          <View style={{opacity: 0.5}}>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[5].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[6].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[7].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[8].word}</Text>
+          <View style={{opacity: 0.8}}>
+            <Text style={[styles.wc2, {top: 55, right: 15}]}>{words[1].word}</Text>
+            <Text style={[styles.wc2, {top: 85, left: 15}]}>{words[2].word}</Text>
+            <Text style={[styles.wc2, {top: -95, left: -10}]}>{words[3].word}</Text>
+            <Text style={[styles.wc2, {top: -100, right: 40}]}>{words[4].word}</Text>
           </View>
-          <View style={{opacity: 0.35}}>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[9].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[10].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[11].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[12].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[13].word}</Text>
-            <Text style={{color: 'white', backgroundColor:'transparent'}}>{words[14].word}</Text>
+          <View style={{opacity: 0.6}}>
+            <Text style={[styles.wc3, {top: 10, right: 10}]}>{words[5].word}</Text>
+            <Text style={[styles.wc3, {top: -25, left: 10}]}>{words[6].word}</Text>
+            <Text style={[styles.wc3, {top: -50, right: -10}]}>{words[7].word}</Text>
+            <Text style={[styles.wc3, {top: 45, left: 30}]}>{words[8].word}</Text>
+          </View>
+          <View style={{opacity: 0.3}}>
+            <Text style={[styles.wc4, {top: -60, right: 150}]}>{words[9].word}</Text>
+            <Text style={[styles.wc4, {top: 80, right: 170}]}>{words[10].word}</Text>
+            <Text style={[styles.wc4, {top: -45, left: 80}]}>{words[11].word}</Text>
+            <Text style={[styles.wc4, {top: 20, left: -10}]}>{words[12].word}</Text>
+            <Text style={[styles.wc4, {top: -90, left: 130}]}>{words[13].word}</Text>
+            <Text style={[styles.wc4, {top: -15, right: 60}]}>{words[14].word}</Text>
           </View>
         </WordCloud>
 
@@ -163,6 +165,29 @@ const styles = StyleSheet.create({
   wc1: {
     color: 'white',
     backgroundColor:'transparent',
+    fontSize: 66,
+    fontWeight: '200',
+  },
+  wc2: {
+    color: 'white',
+    backgroundColor:'transparent',
+    fontSize: 42,
+    fontWeight: '200',
+    position: 'absolute',
+  },
+  wc3: {
+    color: 'white',
+    backgroundColor:'transparent',
+    fontSize: 30,
+    fontWeight: '200',
+    position: 'absolute',
+  },
+  wc4: {
+    color: 'white',
+    backgroundColor:'transparent',
+    fontSize: 18,
+    fontWeight: '200',
+    position: 'absolute',
   },
   statisticsContainer: {
     flex: 1,
