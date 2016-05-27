d7f0abd9577e70abb1a328fb9bcc88c4f4419f20
Jonathan Younger <jonathan@daikini.com>
2016-05-27 08:18:35 -0600

    Android WIP

---
+++ App/app/Components/Common/ParallaxMotionView.android.js
@@ -0,0 +1,13 @@
+/* @flow */
+'use strict';
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class ParallaxMotionView extends Component {
+  render() {
+    return (
+      <View {...this.props} />
+    );
+  }
+};
