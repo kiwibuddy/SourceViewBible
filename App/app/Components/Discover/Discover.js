5c7812fb1f0e7ffd025767da2f34721801d2a06b
Jonathan Younger <jonathan@daikini.com>
2016-05-30 15:38:37 -0600

    Sort discovery books by source count

--- App/app/Components/Discover/Discover.js
+++ App/app/Components/Discover/Discover.js
@@ -51,7 +51,7 @@ class Discover extends Component {
   }
 
   componentDidMount() {
-    const books = Bible.slice(0, MAXIMUM_BOOK_COUNT);
+    const books = Bible.sort((a, b) => a.sourceCount > b.sourceCount ? -1 : 1).slice(0, MAXIMUM_BOOK_COUNT);
 
     this.setState({
       dataSource: this.state.dataSource.cloneWithRows(books)
