22581591e209716c54d927ca51135e3f9e477eea
Jonathan Younger <jonathan@daikini.com>
2016-05-31 11:27:17 -0600

    Emdros WIP

--- App/app/SourceViewBible.js
+++ App/app/SourceViewBible.js
@@ -1,16 +1,22 @@
 import App from './Components/App';
-import React from 'React';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './Store/configureStore';
 
+import Emdros from './API/Emdros';
+
 const store = configureStore();
 
-const SourceViewBible = () => {
+export default class SourceViewBible extends Component {
+  componentDidMount() {
+    Emdros.openDatabase();
+  }
+
+  render() {
     return (
       <Provider store={store}>
         <App />
       </Provider>
     );
+  }
 }
-
-export default SourceViewBible;
