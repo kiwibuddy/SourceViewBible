b761c943b94118d9e28e16cf4a6b6f32da6e4399
Jonathan Younger <jonathan@daikini.com>
2016-06-01 12:22:20 -0600

    Emdros WIP

--- Libraries/Emdros/ios/RCTEmdros/RCTEmdros/RCTEmdrosEnv.mm
+++ Libraries/Emdros/ios/RCTEmdros/RCTEmdros/RCTEmdrosEnv.mm
@@ -50,84 +50,78 @@
         _emdrosEnv = new EmdrosEnv(output_kind, kCSUTF8, hostname, user, password, initial_db, backend_kind);
     }
 
-    dispatch_async(self.queue, ^{
-        BOOL isConnected = _emdrosEnv->connectionOk() == true;
-        if (completion) completion(isConnected, nil);
-    });
+    BOOL isConnected = _emdrosEnv->connectionOk() == true;
+    if (completion) completion(isConnected, nil);
 }
 
 - (void)query:(NSString *)query options:(NSDictionary *)options completion:(void (^)(id result, NSError *error))completion {
-    dispatch_async(self.queue, ^{
-        try {
-            if ([options[@"count"] boolValue]) {
-               [[OCDBenchmark sharedBenchmark] begin];
-                
-                NSInteger firstMonad = options[@"firstMonad"] ? [options[@"firstMonad"] integerValue] : 1;
-                NSInteger lastMonad = options[@"lastMonad"] ? [options[@"lastMonad"] integerValue] : MAX_MONAD;
-                SetOfMonads substrate(firstMonad, lastMonad);
-                
-                std::string errorMessage;
-                std::string json = countInBuckets(_emdrosEnv, std::string(query.UTF8String), substrate, errorMessage);
-                [[OCDBenchmark sharedBenchmark] end:[NSString stringWithFormat:@"%@ countInBuckets", query]];
-                
-                [[OCDBenchmark sharedBenchmark] begin];
-                NSString *data = [NSString stringWithUTF8String:json.c_str()];
-                NSError *error = nil;
-                NSDictionary *result = [NSJSONSerialization JSONObjectWithData:[data dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&error];
-                [[OCDBenchmark sharedBenchmark] end:[NSString stringWithFormat:@"%@ JSONObjectWithData", query]];
-                
-                if (completion) completion(result, nil);
-            } else {
-                
-            }
-        } catch (EMdFDBException e) {
-            std::cerr << "ERROR: EMdFDBException (Database error)..." << std::endl;
-            std::cerr << _emdrosEnv->getDBError() << std::endl;
-            std::cerr << _emdrosEnv->getCompilerError() << std::endl;
-        } catch (BadMonadsException e) {
-            std::cerr << "BadMonadsException caught.  Program aborted." << std::endl;
-        } catch (WrongCharacterSetException e) {
-            std::cerr << "WrongCharacterSetException caught.  Program aborted." << std::endl;
-        } catch (EMdFOutputException e) {
-            std::cerr << "EMdFOutputException caught.  Program aborted." << std::endl;
-        } catch (EmdrosException e) {
-            std::cerr << "ERROR: EmdrosException (Emdros error)..." << e.what() << std::endl;
-        } catch (...) {
-            std::cerr << "Unknown exception occurred.  Program aborted." << std::endl;
+    try {
+        if ([options[@"count"] boolValue]) {
+           [[OCDBenchmark sharedBenchmark] begin];
+            
+            NSInteger firstMonad = options[@"firstMonad"] ? [options[@"firstMonad"] integerValue] : 1;
+            NSInteger lastMonad = options[@"lastMonad"] ? [options[@"lastMonad"] integerValue] : MAX_MONAD;
+            SetOfMonads substrate(firstMonad, lastMonad);
+            
+            std::string errorMessage;
+            std::string json = countInBuckets(_emdrosEnv, std::string(query.UTF8String), substrate, errorMessage);
+            [[OCDBenchmark sharedBenchmark] end:[NSString stringWithFormat:@"%@ countInBuckets", query]];
+            
+            [[OCDBenchmark sharedBenchmark] begin];
+            NSString *data = [NSString stringWithUTF8String:json.c_str()];
+            NSError *error = nil;
+            NSDictionary *result = [NSJSONSerialization JSONObjectWithData:[data dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&error];
+            [[OCDBenchmark sharedBenchmark] end:[NSString stringWithFormat:@"%@ JSONObjectWithData", query]];
+            
+            if (completion) completion(result, nil);
+        } else {
+            
         }
-    });
+    } catch (EMdFDBException e) {
+        std::cerr << "ERROR: EMdFDBException (Database error)..." << std::endl;
+        std::cerr << _emdrosEnv->getDBError() << std::endl;
+        std::cerr << _emdrosEnv->getCompilerError() << std::endl;
+    } catch (BadMonadsException e) {
+        std::cerr << "BadMonadsException caught.  Program aborted." << std::endl;
+    } catch (WrongCharacterSetException e) {
+        std::cerr << "WrongCharacterSetException caught.  Program aborted." << std::endl;
+    } catch (EMdFOutputException e) {
+        std::cerr << "EMdFOutputException caught.  Program aborted." << std::endl;
+    } catch (EmdrosException e) {
+        std::cerr << "ERROR: EmdrosException (Emdros error)..." << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception occurred.  Program aborted." << std::endl;
+    }
 }
 
 - (void)stringFrom:(NSInteger)from to:(NSInteger)to options:(NSDictionary *)options completion:(void (^)(id result, NSError *error))completion {
 //    extern std::string render_objects(EmdrosEnv *pEnv, const std::string& db_name, const std::string& JSON_stylesheet, const std::string& stylesheet, monad_m first_monad, monad_m last_monad, bool& bResult);
 
-    dispatch_async(self.queue, ^{
-        try {
-            NSString *stylesheet = ([options[@"stylesheet"] isKindOfClass:[NSString class]] ? options[@"stylesheet"] : [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:options[@"stylesheet"] options:0 error:nil] encoding:NSUTF8StringEncoding]);
+    try {
+        NSString *stylesheet = ([options[@"stylesheet"] isKindOfClass:[NSString class]] ? options[@"stylesheet"] : [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:options[@"stylesheet"] options:0 error:nil] encoding:NSUTF8StringEncoding]);
 
-            std::string dbName("");
-            std::string stylesheetString(stylesheet.UTF8String);
-            std::string stylesheetName("base");
-            bool bResult;
-            std::string rendered_objects = render_objects(_emdrosEnv, dbName, stylesheetString, stylesheetName, from, to, bResult);
-            NSString *string = [NSString stringWithUTF8String:rendered_objects.c_str()];
-            if (completion) completion(string, nil);
-        } catch (EMdFDBException e) {
-            std::cerr << "ERROR: EMdFDBException (Database error)..." << std::endl;
-            std::cerr << _emdrosEnv->getDBError() << std::endl;
-            std::cerr << _emdrosEnv->getCompilerError() << std::endl;
-        } catch (BadMonadsException e) {
-            std::cerr << "BadMonadsException caught.  Program aborted." << std::endl;
-        } catch (WrongCharacterSetException e) {
-            std::cerr << "WrongCharacterSetException caught.  Program aborted." << std::endl;
-        } catch (EMdFOutputException e) {
-            std::cerr << "EMdFOutputException caught.  Program aborted." << std::endl;
-        } catch (EmdrosException e) {
-            std::cerr << "ERROR: EmdrosException (Emdros error)..." << e.what() << std::endl;
-        } catch (...) {
-            std::cerr << "Unknown exception occurred.  Program aborted." << std::endl;
-        }
-    });
+        std::string dbName("");
+        std::string stylesheetString(stylesheet.UTF8String);
+        std::string stylesheetName("base");
+        bool bResult;
+        std::string rendered_objects = render_objects(_emdrosEnv, dbName, stylesheetString, stylesheetName, from, to, bResult);
+        NSString *string = [NSString stringWithUTF8String:rendered_objects.c_str()];
+        if (completion) completion(string, nil);
+    } catch (EMdFDBException e) {
+        std::cerr << "ERROR: EMdFDBException (Database error)..." << std::endl;
+        std::cerr << _emdrosEnv->getDBError() << std::endl;
+        std::cerr << _emdrosEnv->getCompilerError() << std::endl;
+    } catch (BadMonadsException e) {
+        std::cerr << "BadMonadsException caught.  Program aborted." << std::endl;
+    } catch (WrongCharacterSetException e) {
+        std::cerr << "WrongCharacterSetException caught.  Program aborted." << std::endl;
+    } catch (EMdFOutputException e) {
+        std::cerr << "EMdFOutputException caught.  Program aborted." << std::endl;
+    } catch (EmdrosException e) {
+        std::cerr << "ERROR: EmdrosException (Emdros error)..." << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception occurred.  Program aborted." << std::endl;
+    }
 }
 
 - (NSString *)monadSetForBook:(NSString *)book {
