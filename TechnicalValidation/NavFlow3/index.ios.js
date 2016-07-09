20c43d3cafefab02dcaa8bb38dc983aa7527b407
Jonathan Younger <jonathan@daikini.com>
2016-07-09 11:28:29 -0600

    NavFlow3

--- TechnicalValidation/NavFlow3/index.ios.js
+++ TechnicalValidation/NavFlow3/index.ios.js
@@ -17,6 +17,10 @@ import {
 
 const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
 
+function randomColor() {
+  return '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+}
+
 class NavFlow3 extends Component {
   state = {
   navigation: {
@@ -144,7 +148,7 @@ class ExampleScene extends Component {
     const {style, route, onPress} = this.props;
     return (
       <View style={style}>
-        <TouchableHighlight onPress={() => onPress({backgroundColor: 'red'})}>
+        <TouchableHighlight onPress={() => onPress({backgroundColor: randomColor()})}>
           <Text style={{color:'white'}}>{route.key}</Text>
         </TouchableHighlight>
       </View>
