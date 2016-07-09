0bc12afb8144d1b8a4d2df4a900cd59b6065f78f
Jonathan Younger <jonathan@daikini.com>
2016-07-09 12:17:23 -0600

    Scenes

---
+++ App/app/Scenes/DiscoveryCenter/DiscoveryCenter.js
@@ -0,0 +1,9 @@
+/* @flow */
+'use strict';
+
+import React, { Component, PropTypes } from 'react';
+const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+
+import {
+  View
+} from 'react-native';
