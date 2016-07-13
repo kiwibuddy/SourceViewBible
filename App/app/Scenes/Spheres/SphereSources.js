c23298599974c13f8ca5765b275a7855107fe537
Harold <harold@overcommittedapps.com>
2016-07-13 16:46:57 -0700

    Sphere Sources

--- App/app/Scenes/Spheres/SphereSources.js
+++ App/app/Scenes/Spheres/SphereSources.js
@@ -6,7 +6,8 @@ const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithP
 
 import {
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -15,11 +16,51 @@ import {
   Localizable
 } from '../../Common';
 
+import Icon from '../../Components/Common/Icon';
+
 export default class SphereSources extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>SphereSources</Text>
+        <View style={StyleSheet.styles.statisticsContainer}>
+          <View style={StyleSheet.styles.statisticContainer}>
+            <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
+            <Text style={StyleSheet.styles.statisticSubtitle}>All Sources</Text>
+          </View>
+          <View style={StyleSheet.styles.statisticKeyline} />
+          <View style={StyleSheet.styles.statisticContainer}>
+            <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
+            <Text style={StyleSheet.styles.statisticSubtitle}>OT Sources</Text>
+          </View>
+          <View style={StyleSheet.styles.statisticKeyline} />
+          <View style={StyleSheet.styles.statisticContainer}>
+            <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
+            <Text style={StyleSheet.styles.statisticSubtitle}>NT Sources</Text>
+          </View>
+          <View style={StyleSheet.styles.statisticKeyline} />
+        </View>
+        <View style={styles.sphereBooksGraph}>
+        </View>
+        <TouchableOpacity style={styles.section}>
+          <View style={[styles.sourcesCellContainer, {paddingVertical: 12}]}>
+          <View style={styles.sourcesLeftContainer}>
+            <Icon
+              name={'avatar-human-group'}
+              style={[styles.sourceAvatar, {color: 'red', marginRight: 5}]}
+              size={20}
+            />
+            <Text style={StyleSheet.styles.cell.titlemedium}>Source Name</Text>
+          </View>
+            <View style={styles.sourcesRightContainer}>
+              <View style={[styles.sourcesBarChart, {height: 4, backgroundColor: '#EDEDED'}]} />
+              <View style={styles.dataPair}>
+                <Text style={[StyleSheet.styles.cell.percentage, {color: 'red'}]}>0%</Text>
+                <Text style={StyleSheet.styles.cell.subtitle}>0 words</Text>
+              </View>
+            </View>
+          </View>
+          <View style={[StyleSheet.styles.separator]}></View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -28,5 +69,52 @@ export default class SphereSources extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  sphereBooksGraph: {
+    height: 200,
+    borderBottomColor: Colors.separator,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    shadowColor: "black",
+    shadowOpacity: 0.05,
+    shadowRadius: 0.4,
+    shadowOffset: {
+      height: 1,
+      width: 0
+    },
+  },
+  sectionHeaderContainer: {
+    ...StyleSheet.styles.sectionHeaderContainer,
+    marginLeft: 8,
+    backgroundColor: 'transparent',
+  },
+  section: {
+    marginLeft: 8,
+  },
+  sourcesCellContainer: {
+    flex: 1,
+    marginRight: 15,
+    flexDirection: 'row',
+  },
+  sourcesLeftContainer: {
+    flex: 1.5,
+    flexDirection: 'row',
+    paddingTop: 2,
+  },
+  sourcesRightContainer: {
+    flex: 2,
+  },
+  sourceAvatar: {
+    width: 20,
+    height: 20,
+    marginRight: 5
+  },
+  sourcesBarChart: {
+    height: 4,
+    flex: 0,
+    marginBottom: 7,
+  },
+  dataPair: {
+    flex: 1,
+    flexDirection: 'row',
   },
 });
