93611272045cec17a3e96a37fd7454f74196191a
Jonathan Younger <jonathan@daikini.com>
2016-07-13 17:16:11 -0600

    Book Spheres

--- App/app/Scenes/Books/BookSpheres.js
+++ App/app/Scenes/Books/BookSpheres.js
@@ -15,6 +15,8 @@ import {
   Localizable
 } from '../../Common';
 
+import { SourcesBarChart, SpheresBarChart } from '../../Components/Charts';
+
 type Props = {
   book: Object,
 };
@@ -27,14 +29,19 @@ export default class BookSpheres extends Component {
       <View style={styles.container}>
         <View style={StyleSheet.styles.statisticsContainer}>
           <View style={StyleSheet.styles.statisticContainer}>
-            <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
+            <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(book.sphereWordCount, {precision: 0})}</Text>
             <Text style={StyleSheet.styles.statisticSubtitle}>Words</Text>
           </View>
           <View style={StyleSheet.styles.statisticKeyline} />
           <View style={StyleSheet.styles.statisticContainer}>
             <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-              <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
-              <View style={styles.SpheresBarChart}></View>
+              <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(book.sphereCount, {precision: 0})}</Text>
+              <SpheresBarChart
+                style={{flex: 0, marginLeft: 4}}
+                barStyle={{width: 4, height: 24, marginHorizontal: 1}}
+                horizontal={false}
+                data={[{family: book.sphereCounts.family}, {economics: book.sphereCounts.economics}, {government: book.sphereCounts.government}, {religion: book.sphereCounts.religion}, {education: book.sphereCounts.education}, {communication: book.sphereCounts.communication}, {celebration: book.sphereCounts.celebration}]}
+              />
             </View>
             <Text style={StyleSheet.styles.statisticSubtitle}>Spheres</Text>
           </View>
