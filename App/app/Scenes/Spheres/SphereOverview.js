b66f44b9cb05a021fe839d2beab28ba09e4ed60b
Harold <harold@overcommittedapps.com>
2016-07-13 14:39:46 -0700

    Spheres Overview WIP

--- App/app/Scenes/Spheres/SphereOverview.js
+++ App/app/Scenes/Spheres/SphereOverview.js
@@ -19,7 +19,11 @@ export default class SphereOverview extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>SphereOverview</Text>
+        <View style={styles.carouselContainer}>
+        </View>
+        <View style={styles.carouselGraphContainer}>
+          <View style={[styles.carouselGraph, {width: 150}]} />
+        </View>
       </View>
     );
   }
@@ -28,5 +32,17 @@ export default class SphereOverview extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  carouselContainer: {
+    height: 250,
+    backgroundColor: '#E1E9EE',
+  },
+  carouselGraphContainer: {
+    height: 2,
+    backgroundColor: '#D8D8D8',
+  },
+  carouselGraph: {
+    height: 2,
+    backgroundColor: 'red',
   },
 });
