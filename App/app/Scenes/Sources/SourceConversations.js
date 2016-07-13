7a891463830e7d2d4b6da2bcb61b41030abc02ca
Harold <harold@overcommittedapps.com>
2016-07-13 09:50:17 -0700

    Conversations WIP

--- App/app/Scenes/Sources/SourceConversations.js
+++ App/app/Scenes/Sources/SourceConversations.js
@@ -6,7 +6,8 @@ const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithP
 
 import {
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -15,11 +16,59 @@ import {
   Localizable
 } from '../../Common';
 
+import SourcesBarChart from '../../Components/Charts/SourcesBarChart';
+
+import Icon from '../../Components/Common/Icon';
+
 export default class SourceConversation extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>SourceConversation</Text>
+        <View style={StyleSheet.styles.statisticsContainer}>
+          <View style={StyleSheet.styles.statisticContainer}>
+            <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
+            <Text style={StyleSheet.styles.statisticSubtitle}>Book</Text>
+          </View>
+          <View style={StyleSheet.styles.statisticKeyline} />
+          <View style={StyleSheet.styles.statisticContainer}>
+            <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+              <Text style={StyleSheet.styles.statisticTitleBold}>{Localizable.toNumber(0, {precision: 0})}</Text>
+              <View style={styles.SpheresBarChart}></View>
+            </View>
+            <Text style={StyleSheet.styles.statisticSubtitle}>Spheres</Text>
+          </View>
+        </View>
+        <View style={styles.conversationGraph}>
+        </View>
+        <TouchableOpacity style={styles.section}>
+          <View style={[styles.sourcesCellContainer, {paddingVertical: 15}]}>
+            <View style={styles.sourcesLeftContainer}>
+              <TouchableOpacity onPress={() => this.props.onPressSource(source)}>
+                <Icon
+                  name={'avatar-human-group'}
+                  style={[styles.sourceAvatar, {color: 'black'}]}
+                  size={20}
+                />
+              </TouchableOpacity>
+              <Text style={StyleSheet.styles.cell.title}>Source Name</Text>
+            </View>
+            <View style={styles.sourcesRightContainer}>
+              <View styl={styles.sourcesBarChart} />
+              <Text style={StyleSheet.styles.cell.subtitle}>0 conversations</Text>
+            </View>
+          </View>
+          <View style={[StyleSheet.styles.separator, {marginLeft: -15}]}></View>
+        </TouchableOpacity>
+        <View style={styles.sourceOccurences}>
+          <View style={styles.listItemContainer}>
+            <Text style={StyleSheet.styles.cell.occurence}>1</Text>
+            <View style={styles.listItem}>
+              <Text style={styles.body}>Lorem ipsum dolor sit amet, eleifend varius. Risus vitae mauris cras lectus ipsum ante, semper id, tincidunt nunc magnis vehicula magnis in, magna massa, lectus donec vestibulum interdum.</Text>
+              <Text style={StyleSheet.styles.cell.subtitle}>Genesis 1:1</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
+        </View>
       </View>
     );
   }
@@ -28,5 +77,65 @@ export default class SourceConversation extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  section: {
+    marginLeft: 15,
+  },
+  sourcesCellContainer: {
+    flex: 1,
+    marginRight: 15,
+    flexDirection: 'row',
+  },
+  sourcesLeftContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    paddingTop: 2,
+  },
+  sourcesRightContainer: {
+    flex: 1,
+  },
+  sourceAvatar: {
+    width: 20,
+    height: 20,
+    marginRight: 5
+  },
+  sourcesBarChart: {
+    height: 4,
+    flex: 0,
+    marginBottom: 7,
+  },
+  listItemContainer: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    backgroundColor: '#F9F9F9',
+  },
+  listItemHeader: {
+    borderTopWidth: 2,
+    borderTopColor: 'green',
+    paddingLeft: 15,
+    paddingVertical: 8,
+    borderBottomColor: Colors.separator,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    shadowColor: "black",
+    shadowOpacity: 0.05,
+    shadowRadius: 0.4,
+    shadowOffset: {
+      height: 1,
+      width: 0
+    },
+  },
+  listItem: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingLeft: 8,
+  },
+  separator: {
+    ...StyleSheet.styles.separator,
+  },
+  body: {
+    ...StyleSheet.styles.body,
+    paddingBottom: 5,
   },
 });
