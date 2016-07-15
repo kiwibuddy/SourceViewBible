46432e0e50490fd14c6c12cd41fbbd6fa113790f
Jonathan Younger <jonathan@daikini.com>
2016-07-15 10:43:37 -0600

    Refactor SourcesBarChart to use BarChart

--- App/app/Components/Charts/SourcesBarChart.js
+++ App/app/Components/Charts/SourcesBarChart.js
@@ -3,6 +3,7 @@
 
 import React, { Component, PropTypes } from 'react';
 import { View } from 'react-native';
+import BarChart from './BarChart';
 
 import StyleSheet from '../../Common/StyleSheet';
 import Colors from '../../Common/Colors';
@@ -10,16 +11,25 @@ import Colors from '../../Common/Colors';
 const SOURCES = ['narrator', 'god', 'lead', 'support'];
 
 const SourcesBarChart = (props: Object) => {
-  const chartStyle = [styles.chart, props.style];
-  const stackedBarStyle = [styles.stackedBar, {flexDirection: props.horizontal ? 'row' : 'column'}, props.barStyle];
-  const sources = (props.horizontal ? SOURCES : SOURCES.slice().reverse());
+  const bars = props.data.map((data) => {
+    const slices = SOURCES.map((source) => {
+      const value = data[source];
+      if (value === undefined) return null;
+      return {
+        color: Colors.sources[source].tint,
+        value: value
+      };
+    }).filter(bar => bar != null);
+
+    return {slices};
+  });
 
   let maxChartValue = props.maxChartValue || 0;
   if (props.maxChartValue == null) {
     props.data.forEach((data, index) => {
       let maxBarValue = 0;
-      sources.forEach((source, index) => {
-          const value = data[source] || 0;
+      SOURCES.forEach((sphere, index) => {
+          const value = data[sphere] || 0;
           maxBarValue += value;
       });
       if (maxBarValue > maxChartValue) {
@@ -28,43 +38,12 @@ const SourcesBarChart = (props: Object) => {
     });
   }
 
-  let barIndex = 0;
-  const bars = props.data.map((data) => {
-    let maxBarValue = 0;
-    const bar = sources.map((source) => {
-      const value = data[source];
-      if (value === undefined) return null;
-      maxBarValue += value;
-
-      const barStyle = {
-        backgroundColor: Colors.sources[source].tint,
-        flex: value
-      }
-      return (
-        <View key={'source-' + source} style={barStyle} />
-      );
-    });
-    if (!bar) return null;
-
-    const delta = maxChartValue - maxBarValue;
-    const deltaBar = delta > 0 ? <View key='deltaBar' style={[{flex: delta}, styles.deltaBar]} /> : null;
-    let chart = null;
-    if (props.horizontal) {
-      chart = [bar, deltaBar];
-    } else {
-      chart = [deltaBar, bar];
-    }
-    return (
-      <View key={'bar-' + barIndex++} style={stackedBarStyle}>
-        {chart}
-      </View>
-    )
-  });
-
   return (
-    <View style={chartStyle}>
-      {bars}
-    </View>
+    <BarChart
+      {...props}
+      bars={bars}
+      maxChartValue={maxChartValue}
+    />
   );
 };
 
@@ -78,18 +57,5 @@ SourcesBarChart.propTypes = {
 SourcesBarChart.defaultProps = {
   horizontal: true
 }
-
-const styles = StyleSheet.create({
-  chart: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  deltaBar: {
-    backgroundColor: '#ededed',
-  },
-  stackedBar: {
-    flex: 1,
-  }
-});
 
 export default SourcesBarChart;
