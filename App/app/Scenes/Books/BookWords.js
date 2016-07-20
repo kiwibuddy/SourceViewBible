ac728ccaf4a242caa8c473b2a1b16d74e05a438d
Jonathan Younger <jonathan@daikini.com>
2016-07-20 09:50:14 -0600

    Refactor Data

--- App/app/Scenes/Books/BookWords.js
+++ App/app/Scenes/Books/BookWords.js
@@ -6,12 +6,12 @@ const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithP
 
 import {
   Image,
-  ListView,
   RecyclerViewBackedScrollView,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
+import { ListView } from '../../Components/Common/DatabaseListView';
 
 import {
   Colors,
@@ -21,6 +21,8 @@ import {
 
 import { SourcesBarChart, SpheresBarChart, WordCloud } from '../../Components/Charts';
 import ParallaxMotionView from '../../Components/Common/ParallaxMotionView';
+
+import { Book } from '../../Database';
 
 type Props = {
   bible: Object,
@@ -40,7 +42,7 @@ export default class BookWords extends Component {
   constructor(props: Props) {
     super(props);
 
-    const book = props.bible.books.find(book => book.id === props.bookID);
+    const book = Book.findByID(props.bookID);
 
     const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id, sectionHeaderHasChanged: (s1, s2) => s1 !== s2});
     this.state = {
@@ -66,15 +68,15 @@ export default class BookWords extends Component {
   _renderRow = (word: Object, sectionID: any, rowID: any) => {
     return (
       <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>{word.word}</Text>
-        <Text style={StyleSheet.styles.cell.valuetitle}>{Localizable.toNumber(word.wordCount, {precision: 0})}</Text>
+        <Text style={StyleSheet.styles.cell.title}>{word.string}</Text>
+        <Text style={StyleSheet.styles.cell.valuetitle}>{Localizable.toNumber(word.count, {precision: 0})}</Text>
       </TouchableOpacity>
     );
   };
 
   _renderHeader = (props: any) => {
     const { book } = this.state;
-    const words = book.words.map(word => word.word);
+    const words = book.words.map(word => word.string);
 
     return (
       <WordCloud
