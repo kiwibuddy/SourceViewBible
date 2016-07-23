c5249b4c0cce71656e344f65a0d73e4122025172
Jonathan Younger <jonathan@daikini.com>
2016-07-23 11:45:10 -0600

    History WIP

--- App/app/Scenes/Bookmarks/Bookmarks.js
+++ App/app/Scenes/Bookmarks/Bookmarks.js
@@ -50,14 +50,20 @@ export default class Bookmarks extends Component {
   constructor(props: Props) {
     super(props);
 
-    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id, sectionHeaderHasChanged: (s1, s2) => s1 !== s2});
     let selectedSegmentIndex = Preference.numberForKey(SEGMENT_PREFERENCE);
     if (selectedSegmentIndex == null) selectedSegmentIndex = SEGMENT_INDEXES.BOOKMARKS;
-    
+
     this.state = {
-      dataSource: dataSource.cloneWithRows(this._getRows()),
+      dataSource: dataSource,
       selectedSegmentIndex
     }
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this._getDataSource(this.state.selectedSegmentIndex)
+    });
   }
 
   _getRows = () => {
@@ -89,6 +95,7 @@ export default class Bookmarks extends Component {
           </TouchableOpacity>
         </NavigationBar>
         <ListView
+          enableEmptySections={true}
           dataSource={this.state.dataSource}
           renderRow={this._renderRow}
           renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={StyleSheet.styles.separator} />}
@@ -98,23 +105,78 @@ export default class Bookmarks extends Component {
     );
   }
 
-  _renderRow = (route: Object) => {
+  _renderRow = (data: Object) => {
+    switch (this.state.selectedSegmentIndex) {
+      case SEGMENT_INDEXES.HISTORY:
+        return this._renderHistoryRow(data);
+
+      case SEGMENT_INDEXES.HIGHLIGHTS:
+        return this._renderHighlightRow(data);
+
+      default:
+        return this._renderBookmarkRow(data);
+    }
+  };
+
+  _renderBookmarkRow = (bookmark: Object) => {
     return (
       <TouchableOpacity
-        onPress={() => this.props.onPressRoute(route)}
+        onPress={() => this.props.onPressRoute(bookmark)}
         style={styles.row}
       >
-        <Image source={route.icon} style={styles.icon} />
-        <Text style={StyleSheet.styles.cell.title}>{route.title}</Text>
+        <Image source={bookmark.icon} style={styles.icon} />
+        <Text style={StyleSheet.styles.cell.title}>{bookmark.title}</Text>
       </TouchableOpacity>
-    )
+    );
+  }
+
+  _renderHistoryRow = (history: Object) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {}}
+        style={styles.row}
+      >
+        <Text style={StyleSheet.styles.cell.title}>{history.title}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  _renderHighlightRow = (history: Object) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {}}
+        style={styles.row}
+      >
+        <Text style={StyleSheet.styles.cell.title}></Text>
+      </TouchableOpacity>
+    );
+  };
+
+  _getDataSource = (segmentIndex: number) => {
+    switch (segmentIndex) {
+      case SEGMENT_INDEXES.HISTORY:
+        return this.state.dataSource.cloneWithRowsAndSections({history: History.all()});
+
+      case SEGMENT_INDEXES.HIGHLIGHTS:
+        return this.state.dataSource.cloneWithRowsAndSections({highlights: []});
+
+      default:
+        const bookmarks = [
+          {key: '/Discover', title: 'Discover', icon: require('../../Images/tabs/discover.png')},
+          {key: '/Books', title: 'Books', icon: require('../../Images/tabs/chapters.png')},
+          {key: '/Sources', title: 'Sources', icon: require('../../Images/tabs/sources.png')},
+          {key: '/Spheres', title: 'Spheres', icon: require('../../Images/tabs/spheres.png')},
+        ];
+        return this.state.dataSource.cloneWithRowsAndSections({bookmarks: bookmarks});
+    }
   };
 
   _onSegmentedControlValueChanged = (value: number) => {
     Preference.setNumberForKey(value, SEGMENT_PREFERENCE);
 
     this.setState({
-      selectedSegmentIndex: value
+      selectedSegmentIndex: value,
+      dataSource: this._getDataSource(value)
     });
   };
 }
