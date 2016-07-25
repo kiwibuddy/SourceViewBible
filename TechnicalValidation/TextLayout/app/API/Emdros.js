52c259237efe63a0fc92da727cad9461dbad75b8
Jonathan Younger <jonathan@daikini.com>
2016-07-25 12:30:06 -0600

    TextLayout Spike

---
+++ TechnicalValidation/TextLayout/app/API/Emdros.js
@@ -0,0 +1,65 @@
+/* @flow */
+'use strict';
+
+import Emdros from 'react-native-emdros';
+let DB = null;
+
+const SCRIPTURE_STYLESHEET = require('./scripture-stylesheet.json');
+
+function openDatabase() {
+  return new Promise((resolve, reject) => {
+    Emdros.open({name: 'Datasets/en/NLT/SourceView.bpt'}).then((emdros) => {
+      DB = emdros;
+      resolve();
+    }).catch((error) => {
+      console.log(error);
+      reject(error);
+    });
+  });
+}
+
+function query(query: string, options: Object) {
+  if (DB == null) {
+    const promise = new Promise((resolve, reject) => {
+      reject('DB is null');
+    });
+    return promise;
+  }
+
+  return DB.query(query, options);
+}
+
+function scripture(options: Object) {
+  const style = {stylesheet: JSON.stringify(SCRIPTURE_STYLESHEET)};
+
+  let monadSet = options.monadSet;
+  if (!monadSet) {
+    const book = options.book;
+    if (book) {
+      const chapterNumber = options.chapterNumber || 1;
+      const chapter = book.chapters[chapterNumber - 1];
+      monadSet = chapter.monadSet;
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    if (DB == null) {
+      reject('DB is null');
+      return;
+    }
+
+    DB.string(monadSet.first, monadSet.last, style).then((result) => {
+      const from = result.indexOf('React.create');
+      const scripture = 'React.createElement(View, {}, ' + result.slice(from, -1) + ')';
+      resolve(scripture);
+    }).catch((error) => {
+      reject(error);
+    });
+  })
+}
+
+module.exports = {
+  openDatabase,
+  query,
+  scripture
+}
---
+++ TechnicalValidation/TextLayout/index.ios.js
@@ -0,0 +1,95 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+ const React = require('react');
+ import { Component } from 'react';
+
+ import ReactNative from 'react-native';
+
+ const {
+   AppRegistry,
+   View,
+   Text,
+   Image,
+   ScrollView,
+   StyleSheet,
+   TouchableOpacity,
+ } = ReactNative;
+
+import Emdros from './app/API/Emdros';
+
+type State = {
+  scripture: any;
+};
+
+class TextLayout extends Component {
+  state: State;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scripture: null
+    };
+  }
+
+  componentDidMount() {
+    Emdros.openDatabase().then(() => {
+      const options = {monadSet: {first: 1, last: 1000}};
+       Emdros.scripture(options).then(scripture => {
+         this.setState({scripture});
+       }).catch(error => {
+         console.log(error);
+       });
+    })
+  }
+
+  render() {
+    if (!this.state.scripture) return null;
+    const scripture = eval(this.state.scripture);
+
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          contentContainerStyle={styles.scriptureContainer}
+        >
+          {scripture}
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scriptureContainer: {
+    marginVertical: 20,
+    marginHorizontal: 25,
+    paddingBottom: 25,
+  },
+  scripture: {
+    fontFamily: 'Hoefler Text',
+    fontSize: 18,
+    lineHeight: 30,
+  },
+  sourceColorRed: {
+    color: 'red',
+  },
+  sourceColorBlack: {
+    color: 'black',
+  },
+  sourceColorGreen: {
+    color: 'green',
+  },
+  sourceColorBlue: {
+    color: 'blue'
+  },
+});
+
+AppRegistry.registerComponent('TextLayout', () => TextLayout);
