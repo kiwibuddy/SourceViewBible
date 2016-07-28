30e1c858fb0a7883a5f0f2164a75d13897425b34
Jonathan Younger <jonathan@daikini.com>
2016-07-28 14:40:03 -0600

    TextLayout WIP

--- TechnicalValidation/TextLayout/index.ios.js
+++ TechnicalValidation/TextLayout/index.ios.js
@@ -23,14 +23,68 @@
 
 import Emdros from './app/API/Emdros';
 
+const Genesis = {
+  first: 1,
+  last: 50638,
+};
+
+const Genesis1 = {
+  first: 1,
+  last: 1005,
+};
+
+const Genesis1_1 = {
+  first: 1,
+  last: 100,
+};
+
+type State = {
+  scripture: any;
+};
+
 class TextLayout extends Component {
+  state: State;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scripture: null
+    };
+  }
+
+  componentDidMount() {
+    Emdros.openDatabase().then(() => {
+      const options = {monadSet: Genesis};
+       Emdros.scripture(options).then(scripture => {
+        //  console.log(scripture);
+         this.setState({scripture});
+       }).catch(error => {
+         console.log(error);
+       });
+    })
+  }
+
   render() {
+    if (this.state.scripture == null) return null;
+
+    return this._renderWebView();
+    // return this._renderPlainText();
+  }
+
+  _renderWebView() {
+    return <WebView
+      decelerationRate="normal"
+      style={styles.container}
+      source={{html: this.state.scripture}}
+    />
+  };
+
+  _renderPlainText() {
     return (
-      <WebView
-        decelerationRate="normal"
-        style={styles.container}
-        source={require('./scripture.html')}
-      />
+      <ScrollView style={styles.container}>
+        <Text>{this.state.scripture}</Text>
+      </ScrollView>
     );
   }
 }
@@ -39,7 +93,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 20,
-    backgroundColor: 'red',
   },
 });
 
