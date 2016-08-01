71214214adb0684a352239d671f5217b3ff5ab17
Harold <harold@overcommittedapps.com>
2016-08-01 07:53:02 -0700

    Polish

--- App/app/Scenes/Reader/HTML.js
+++ App/app/Scenes/Reader/HTML.js
@@ -14,12 +14,11 @@ module.exports = `
     <style type="text/css">
       body {
         font-family: georgia;
-        font-size: 15pt;
+        font-size: 13pt;
         color: #323B43;
         line-height: 26pt;
       }
       table {
-        margin:0;
         padding:0;
         border-spacing: 0;
         border-collapse: collapse;
