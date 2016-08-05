ae27bd2a6372bee0974d27d5d3917c53fa83858c
Jonathan Younger <jonathan@daikini.com>
2016-08-05 07:53:08 -0600

    Kraken

--- Kraken/db/seeds/recipients.rb
+++ Kraken/db/seeds/recipients.rb
@@ -6,7 +6,7 @@ require './db/seeds/gender.rb'
 recipient_statements = []
 EMDROS[:statement_objects].each do |s|
 	s[:mdf_recipients].to_s.strip.split(" ").map{|r| r.to_i}.each do |recipient_id|
-		recipient_statements << {statement_id: s[:object_id_d], recipient_id: recipient_id} if recipient_id > 0
+		DB[:recipient_statements].insert({statement_id: s[:object_id_d], recipient_id: recipient_id}) if recipient_id > 0
 	end
 end
 
