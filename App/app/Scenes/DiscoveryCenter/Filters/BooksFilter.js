f80d6d008411ff9a61170ceea0c59c990f287f3c
Jonathan Younger <jonathan@daikini.com>
2016-08-08 11:59:13 -0600

    Filters WIP

--- App/app/Scenes/DiscoveryCenter/Filters/BooksFilter.js
+++ App/app/Scenes/DiscoveryCenter/Filters/BooksFilter.js
@@ -1,55 +1,161 @@
 /* @flow */
 'use strict';
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+
 import {
-  ScrollView,
+  AsyncStorage,
+  Platform,
+  RecyclerViewBackedScrollView,
   Text,
   TouchableOpacity,
-  View,
+  View
 } from 'react-native';
+import { ListView } from '../../../Components/Common/DatabaseListView';
 
 import {
   Colors,
-  Localizable,
+  Constants,
   StyleSheet,
+  Localizable
 } from '../../../Common';
 
-const Props = {
+// $FlowFixMe: - Flow can't find os module extension
+import SegmentedControl from '../../../Components/Common/SegmentedControl';
+
+const SEGMENTS = [Localizable.t('textual'), Localizable.t('alphabetical')];
+const SEGMENT_INDEXES = {
+  TEXT: 0,
+  ALPHABETICAL: 1,
+};
+
+const LISTVIEW_REF = 'LISTVIEW_REF';
+
+import { Book } from '../../../Database';
+
+type Props = {
   navigate: Function,
 };
 
-const BooksFilter = (props: Props) => {
-  return (
-    <ScrollView style={styles.container}>
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Whole Bible</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Specific Book</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Old Testament</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>New Testament</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-    </ScrollView>
-  );
+type State = {
+  dataSource: any,
+  selectedSegmentIndex: number,
 };
+
+export default class Books extends Component {
+  props: Props;
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+
+    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id, sectionHeaderHasChanged: (s1, s2) => s1 !== s2});
+    this.state = {
+      dataSource: dataSource,
+      selectedSegmentIndex: SEGMENT_INDEXES.TEXT
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this._getDataSource(this.state.selectedSegmentIndex)
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SegmentedControl
+          style={styles.segmentedControl}
+          tintColor={Colors.tint}
+          values={SEGMENTS}
+          selectedIndex={this.state.selectedSegmentIndex}
+          onValueChange={(value) => this._onSegmentedControlValueChanged(SEGMENTS.indexOf(value))}
+        />
+
+        <ListView
+          ref="LISTVIEW_REF"
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+          renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}
+        />
+      </View>
+    );
+  }
+
+  _renderRow = (book: Object, sectionID: any, rowID: any) => {
+    return (
+      <TouchableOpacity key={book.id} style={StyleSheet.styles.listItem} onPress={() => console.log(book.name)}>
+        <Text style={StyleSheet.styles.cell.title}>{book.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  _getDataSource = (segmentIndex: number) => {
+    switch (segmentIndex) {
+      case SEGMENT_INDEXES.ALPHABETICAL:
+        return this.state.dataSource.cloneWithRowsAndSections({alphabetical: Book.all().sorted('name')});
+
+      default:
+        return this.state.dataSource.cloneWithRowsAndSections({textOrder: Book.all().sorted('textOrder')});
+    }
+  };
+
+  _onSegmentedControlValueChanged = (value: number) => {
+    const listView = this.refs[LISTVIEW_REF];
+    listView.scrollTo({y: 0, animated: false});
+
+    this.setState({
+      selectedSegmentIndex: value,
+      dataSource: this._getDataSource(value)
+    });
+  };
+ }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  section: {
+    marginLeft: 8,
+  },
+  cellContainer: {
+    flex: 1,
+    marginRight: 15,
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    flex: 1.5,
+  },
+  rightContainer: {
+    flex: 2,
+  },
+  stackedBarChart: {
+    height: 4,
+    flex: 0,
+    marginTop: 7,
+  },
   separator: {
     ...StyleSheet.styles.separator,
-    marginLeft: 15,
+    marginLeft: 8,
   },
+  ...Platform.select({
+      ios: {
+        segmentedControl: {
+          marginTop: 8,
+          marginHorizontal: 8,
+          marginBottom: 8,
+        },
+      },
+      android: {
+        segmentedControl: {
+          shadowColor: 'red',
+          elevation: 2,
+        },
+      },
+  })
 });
-
-export default BooksFilter;
