2b0697a0b73754bd8dc39367e537deeb75df6c6d
Jonathan Younger <jonathan@daikini.com>
2016-08-09 10:14:01 -0600

    Chart WIP

--- App/app/Scenes/DiscoveryCenter/Charts/AxisItems.js
+++ App/app/Scenes/DiscoveryCenter/Charts/AxisItems.js
@@ -1,18 +1,25 @@
 /* @flow */
 'use strict';
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+
 import {
-  ScrollView,
+  AsyncStorage,
+  Platform,
+  RecyclerViewBackedScrollView,
   Text,
+  TextInput,
   TouchableOpacity,
-  View,
+  View
 } from 'react-native';
+import { ListView } from '../../../Components/Common/DatabaseListView';
 
 import {
   Colors,
-  Localizable,
+  Constants,
   StyleSheet,
+  Localizable
 } from '../../../Common';
 
 type Props = {
@@ -20,28 +27,94 @@ type Props = {
   onDone: Function,
 };
 
-const AxisItems = (props: Props) => {
-  return (
-    <ScrollView style={styles.container}>
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Foo</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Bar</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Baz</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-        <Text style={StyleSheet.styles.cell.title}>Buz</Text>
-      </TouchableOpacity>
-      <View style={styles.separator} />
-    </ScrollView>
-  );
+type State = {
+  dataSource: any,
+  search: string,
 };
+
+export default class AxisItems extends Component {
+  props: Props;
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+
+    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2, sectionHeaderHasChanged: (s1, s2) => s1 !== s2});
+    this.state = {
+      dataSource: dataSource,
+      search: null,
+    };
+  }
+
+  render() {
+    const { rows, sections} = this._getRowsAndSections();
+    const dataSource = this.state.dataSource.cloneWithRowsAndSections(rows, sections);
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCapitalize="words"
+          autoCorrect={false}
+          autoFocus={true}
+          clearButtonMode="always"
+          onChangeText={(text) => this.setState({search: text})}
+          placeholder="Data Point"
+          style={styles.textInput}
+          value={this.state.search}
+        />
+        <ListView
+          dataSource={dataSource}
+          renderRow={this._renderRow}
+          renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+          renderSectionHeader={this._renderSectionHeader}
+          renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}
+          keyboardShouldPersistTaps={true}
+          keyboardDismissMode="on-drag"
+        />
+      </View>
+    );
+  }
+
+  _renderSectionHeader = (sectionData: Object, sectionID: any) => {
+    const title = Localizable.t(sectionID);
+    return (
+      <View style={styles.sectionHeaderContainer}>
+        <Text style={styles.sectionHeaderTitle}>{title}</Text>
+      </View>
+    );
+  };
+
+  _renderRow = (item: Object, sectionID: any, rowID: any) => {
+    return (
+      <TouchableOpacity key={sectionID + '-' + item.id} style={StyleSheet.styles.listItem} onPress={() => {}}>
+        <Text style={StyleSheet.styles.cell.title}>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  _getRowsAndSections = () => {
+    const { search } = this.state;
+
+    const actantRows = [
+      {id: 'name', name: 'Name'},
+      {id: 'gender', name: 'Gender'},
+      {id: 'nature', name: 'Nature'},
+      {id: 'profession', name: 'Profession'},
+      {id: 'role', name: 'Role'},
+    ];
+
+    const rows = {
+      source: actantRows,
+      recipient: actantRows,
+    };
+
+    const sections = [
+      'source',
+      'recipient'
+    ];
+
+    return {rows, sections};
+  };
+ }
 
 const styles = StyleSheet.create({
   container: {
@@ -49,8 +122,31 @@ const styles = StyleSheet.create({
   },
   separator: {
     ...StyleSheet.styles.separator,
+    marginLeft: 8,
+  },
+  textInput: {
+    fontSize: 14,
+    backgroundColor: '#ececec',
+    borderColor: '#ececec',
+    borderRadius: 3,
+    borderWidth: 1,
+    paddingLeft: 8,
+    marginHorizontal: 8,
+    marginVertical: 8,
+    height: 26,
+  },
+  sectionHeaderContainer: {
+    paddingVertical: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#FAFAFA',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#c8c7cc',
+  },
+  sectionHeaderTitle: {
+    color: '#59626a',
+    fontSize: 15,
+    fontWeight: 'bold',
     marginLeft: 15,
   },
 });
-
-export default AxisItems;
