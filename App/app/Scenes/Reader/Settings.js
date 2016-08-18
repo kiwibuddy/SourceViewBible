0713647357f2a677fbef456666b2105abba3ac47
Jonathan Younger <jonathan@daikini.com>
2016-08-18 14:36:50 -0600

    Settings WIP

--- App/app/Scenes/Reader/Settings.js
+++ App/app/Scenes/Reader/Settings.js
@@ -9,6 +9,7 @@ import {
   Switch,
   Text,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 
@@ -33,6 +34,7 @@ type Props = {
 
 type State = {
   fontSize: number,
+  spheres: Array<string>,
 };
 
 export default class Settings extends Component {
@@ -43,6 +45,7 @@ export default class Settings extends Component {
     super(props);
     this.state = {
       fontSize: 17,
+      spheres: []
     }
   }
 
@@ -59,7 +62,7 @@ export default class Settings extends Component {
           />
           <NavigationBarButton
             title={Localizable.t('done')}
-            onPress={() => this.props.navigate(BACK)}
+            onPress={this._onDone}
             style={{position: 'absolute', right: 0}}
             titleStyle={[StyleSheet.styles.navigationBar.doneButtonTitle, {marginRight: 8}]}
           />
@@ -91,84 +94,77 @@ export default class Settings extends Component {
             <Text style={styles.sectionHeaderTitle}>{Localizable.t('spheres.text').toLocaleUpperCase()}</Text>
           </View>
           <View style={[styles.section, {marginTop: 8}]}>
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-family-filled"
-                size={30}
-                style={{color: Colors.spheres['family'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('family')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('family')}
             <View style={StyleSheet.styles.separator} />
 
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-economics-filled"
-                size={30}
-                style={{color: Colors.spheres['economics'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('economics')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('economics')}
             <View style={StyleSheet.styles.separator} />
 
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-government-filled"
-                size={30}
-                style={{color: Colors.spheres['government'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('government')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('government')}
             <View style={StyleSheet.styles.separator} />
 
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-religion-filled"
-                size={30}
-                style={{color: Colors.spheres['religion'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('religion')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('religion')}
             <View style={StyleSheet.styles.separator} />
 
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-education-filled"
-                size={30}
-                style={{color: Colors.spheres['education'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('education')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('education')}
             <View style={StyleSheet.styles.separator} />
 
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-communication-filled"
-                size={30}
-                style={{color: Colors.spheres['communication'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('communication')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('communication')}
             <View style={StyleSheet.styles.separator} />
 
-            <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => {}}>
-              <Icon
-                name="icon-celebration-filled"
-                size={30}
-                style={{color: Colors.spheres['celebration'].tint}}
-              />
-              <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t('celebration')}</Text>
-            </TouchableOpacity>
+            {this._renderSphereRow('celebration')}
           </View>
         </ScrollView>
       </View>
     );
   }
 
+  _renderSphereRow = (sphere: string) => {
+    const { spheres } = this.state;
+
+    const selected = spheres.indexOf(sphere) != -1;
+    const iconName = `icon-${sphere}-filled`;
+    const statusStyle = (selected ? styles.statusLabelSelected : {});
+    const status = (selected ? Localizable.t('added') : Localizable.t('add'));
+    return (
+      <TouchableWithoutFeedback onPress={() => this._onPressSphere(sphere)}>
+        <View style={StyleSheet.styles.listItem}>
+          <Icon
+            name={iconName}
+            size={30}
+            style={{color: Colors.spheres[sphere].tint}}
+          />
+          <Text style={[StyleSheet.styles.cell.title]}>{Localizable.t(sphere)}</Text>
+          <Text style={[styles.statusLabel, statusStyle]}>{status}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
   _onFontSizeChanged = (step: number) => {
     const increment = (step/1) * 4;
     const fontSize = BASE_FONT_SIZE + (FONT_STEP_SIZE * increment);
     this.setState({fontSize});
+  };
+
+  _onPressSphere = (sphere: string) => {
+    const { spheres } = this.state;
+
+    const sphereIndex = spheres.indexOf(sphere);
+    if (sphereIndex == -1) {
+      this.setState({
+        spheres: [...spheres, sphere]
+      });
+    } else {
+      spheres.splice(sphereIndex, 1);
+      this.setState({
+        spheres
+      });
+    }
+  };
+
+  _onDone = () => {
+    this.props.navigate(BACK);
   };
 };
 
@@ -209,5 +205,16 @@ const styles = StyleSheet.create({
   },
   fontSample: {
     paddingHorizontal: 8,
-  }
+  },
+  statusLabel: {
+    fontSize: 17,
+    color: Colors.tint,
+  },
+  statusLabelSelected: {
+    color: 'white',
+    backgroundColor: Colors.tint,
+    borderRadius: 4,
+    padding: 4,
+    overflow: 'hidden',
+  },
 });
