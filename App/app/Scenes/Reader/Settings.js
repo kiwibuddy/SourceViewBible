b6931697b75f47eea971e9ec1aebaa277f821349
Jonathan Younger <jonathan@daikini.com>
2016-08-18 12:36:27 -0600

    Settings WIP

--- App/app/Scenes/Reader/Settings.js
+++ App/app/Scenes/Reader/Settings.js
@@ -1,2 +1,43 @@
 /* @flow */
 'use strict';
+
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
+
+import {
+  Colors,
+  Localizable,
+  StyleSheet,
+} from '../../Common';
+
+type Props = {
+  navigate: Function,
+  onDone: Function,
+};
+
+const Settings = (props: Props) => {
+  return (
+    <ScrollView style={styles.container}>
+
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  separator: {
+    ...StyleSheet.styles.separator,
+    marginLeft: 15,
+  },
+});
+
+export default Settings;
