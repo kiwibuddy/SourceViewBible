54a36e1bf6b91b3a46a09ddfcd18a516c40ddcca
Jonathan Younger <jonathan@daikini.com>
2016-08-18 16:52:18 -0600

    Sphere WIP

--- App/app/API/Emdros.js
+++ App/app/API/Emdros.js
@@ -7,6 +7,36 @@ let DB = null;
 const SCRIPTURE_STYLESHEET = require('./scripture-stylesheet.json');
 const OCCURRENCE_STYLESHEET = require('./occurrence-stylesheet.json');
 
+// "Token" : {
+//    "docindexfeature" : "docindex",
+//    "end" : "",
+//    "get" : [
+//          "surface",
+//          "sphere_color_number",
+//          "Family",
+//          "Economics",
+//          "Government",
+//          "Religion",
+//          "Education",
+//          "MediaCom",
+//          "Celebration",
+//          "black_override"
+//       ],
+//    "start" : "{{ if varequal 'EndParagraph' 'true' }}</p>{{ setvar 'EndParagraph' }}{{ setvarend }}{{ setvar 'css' }}{{ setvarend }}{{ endif }}{{ if varequal 'StartParagraph' 'true' }}<p class="{{ emitvar 'ParagraphType' }}">{{ setvar 'StartParagraph' }}{{ setvarend }}{{ if varequal 'TextKind' 'Prose' }}{{ setvar 'ParagraphType' }}FlushLeft{{ setvarend }}{{ endif }}{{ endif }}{{ setvar 'new_css' }}{{ if featureequal 1 '0' }}{{ else }}{{ if featureequal 2 'true' }}Fam{{ endif }}{{ if featureequal 3 'true' }}Eco{{ endif }}{{ if featureequal 4 'true' }}Gov{{ endif }}{{ if featureequal 5 'true' }}Rel{{ endif }}{{ if featureequal 6 'true' }}Edu{{ endif }}{{ if featureequal 7 'true' }}Com{{ endif }}{{ if featureequal 8 'true' }}Cel{{ endif }}{{ endif }}{{ setvarend }}{{ if varequal 'new_css' '' }}<span>{{ else }}<span class="spv-{{ emitvar 'new_css' }}">{{ endif }}{{ setvar 'css' }}{{ emitvar 'new_css' }}{{ setvarend }}{{ if varequal 'Italics' 'On' }}<i>{{ endif }}{{ if varequal 'Bold' 'On' }}<b>{{ endif }}{{ if varequal 'SmallCaps' 'On' }}<span class="small-caps">{{ endif }}{{ if featureequal 9 'true' }}<span class="src-clr-Black">{{ endif }}<span class="dmy{{ emitvar 'embedded_document' }}{{ emitvar 'embedded_quotation' }}">{{ feature 0 }}</span>{{ if featureequal 9 'true' }}</span>{{ endif }}{{ if varequal 'SmallCaps' 'On' }}</span>{{ endif }}{{ if varequal 'Bold' 'On' }}</b>{{ endif }}{{ if varequal 'Italics' 'On' }}</i>{{ endif }}</span>"
+// },
+
+function highlightSpheres(spheres: any) {
+  // {{ setvar 'new_css' }}{{ if featureequal 1 '0' }}{{ else }}{{ if featureequal 2 'true' }}Fam{{ endif }}{{ if featureequal 3 'true' }}Eco{{ endif }}{{ if featureequal 4 'true' }}Gov{{ endif }}{{ if featureequal 5 'true' }}Rel{{ endif }}{{ if featureequal 6 'true' }}Edu{{ endif }}{{ if featureequal 7 'true' }}Com{{ endif }}{{ if featureequal 8 'true' }}Cel{{ endif }}{{ endif }}{{ setvarend }}{{ if varequal 'new_css' '' }}<span>{{ else }}<span class="spv-{{ emitvar 'new_css' }}">{{ endif }}
+  const start = `{{ if varequal 'StartParagraph' 'true' }}<p class="{{ emitvar 'ParagraphType' }}">{{ setvar 'StartParagraph' }}{{ setvarend }}{{ if varequal 'TextKind' 'Prose' }}{{ setvar 'ParagraphType' }}{{ setvarend }}{{ endif }}{{ endif }}{{ feature 0 }}`;
+
+  return {
+    "docindexfeature": "docindex",
+    "end": "",
+    "get": ["surface"].concat(spheres),
+    "start": start
+  }
+}
+
 function openDatabase() {
   Emdros.open({name: 'Datasets/en/NLT/SourceView.bpt'}).then((emdros) => {
     DB = emdros;
@@ -29,11 +59,17 @@ function query(query: string, options: Object) {
 function scripture(options: Object) {
   let stylesheet = null;
   if (options && options.stylesheet == 'occurrence') {
-    stylesheet = JSON.stringify(OCCURRENCE_STYLESHEET);
+    stylesheet = OCCURRENCE_STYLESHEET;
   } else {
-    stylesheet = JSON.stringify(SCRIPTURE_STYLESHEET);
+    stylesheet = SCRIPTURE_STYLESHEET;
+
+    if (options && options.spheres) {
+      const base = SCRIPTURE_STYLESHEET['fetchinfo']['base']['object_types'];
+      base['Token'] = highlightSpheres(['Family', 'Education']);
+      console.log('token', SCRIPTURE_STYLESHEET['fetchinfo']['base']['object_types']['Token']);
+    }
   }
-  const style = {stylesheet};
+  const style = {stylesheet: JSON.stringify(stylesheet)};
 
   let monadSet = options.monadSet;
   if (!monadSet) {
