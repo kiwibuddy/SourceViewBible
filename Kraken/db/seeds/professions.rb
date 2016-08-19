adde21bb300e992c956239a0aceff874cea42145
Jonathan Younger <jonathan@daikini.com>
2016-08-19 10:55:40 -0600

    Exclude certain professions from DC search results

--- Kraken/db/seeds/professions.rb
+++ Kraken/db/seeds/professions.rb
@@ -1,7 +1,10 @@
 STDERR.puts "Seeding Professions"
 
+PROFESSIONS = JSON.parse(open("db/seeds/professions.json").read)
+EXCLUDE_PROFESSIONS = PROFESSIONS.select{ |p| p["searchable"] == false }.map{|p| p["id"]}
+
 EMDROS[:actant_objects].each do |actant_object|
-	professions = actant_object[:mdf_professions].to_s.strip.split(' ').select{|c| c.to_i > 0}.map{|c| c.to_i }.uniq
+	professions = actant_object[:mdf_professions].to_s.strip.split(' ').select{|c| c.to_i > 0 && !EXCLUDE_PROFESSIONS.include?(c.to_i)}.map{|c| c.to_i }.uniq
 
   professions.each do |profession_id|
     profession = {
