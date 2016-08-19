d9b7b21559050dd111a6747b32bf6f4c750f923f
Jonathan Younger <jonathan@daikini.com>
2016-08-19 15:41:09 -0600

    Cleanup

--- App/app/Scenes/DiscoveryCenter/Card.js
+++ App/app/Scenes/DiscoveryCenter/Card.js
@@ -210,9 +210,6 @@ export default class Card extends Component {
 
   _renderWordCount = () => {
     const { card, wordCount } = this.state;
-
-    console.log('wordCount', wordCount);
-    
     if (wordCount == null) return null;
 
     const wordFilter = card.filters.find(filter => filter.type === 'word');
