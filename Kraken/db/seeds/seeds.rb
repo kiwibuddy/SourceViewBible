bb3697d9f0cecea14b1913416a6d9f14edb67e1d
Jonathan Younger <jonathan@daikini.com>
2016-08-21 13:26:07 -0600

    Key Passages WIP

--- Kraken/db/seeds/seeds.rb
+++ Kraken/db/seeds/seeds.rb
@@ -1,9 +1,11 @@
 # require './db/seeds/source_actants'
 
-require './db/seeds/actants'
-require './db/seeds/bso'
-require './db/seeds/bso_actants'
-require './db/seeds/professions'
-require './db/seeds/chronologies'
-require './db/seeds/natures'
-require './db/seeds/spheres'
+require './db/seeds/sphere-key-passages'
+
+# require './db/seeds/actants'
+# require './db/seeds/bso'
+# require './db/seeds/bso_actants'
+# require './db/seeds/professions'
+# require './db/seeds/chronologies'
+# require './db/seeds/natures'
+# require './db/seeds/spheres'
---
+++ Kraken/db/seeds/sphere-key-passages.rb
@@ -0,0 +1,233 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'csv'
+require 'pp'
+require 'pericope'
+
+BOOK_TO_DJHREF = {
+  "Genesis" => "Gen",
+  "Gen" => "Gen",
+  "Exodus" => "Exo",
+  "Exo" => "Exo",
+  "Leviticus" => "Lev",
+  "Lev" => "Lev",
+  "Numbers" => "Num",
+  "Num" => "Num",
+  "Deuteronomy" => "Deu",
+  "De" => "De",
+  "Joshua" => "Jos",
+  "Jos" => "Jos",
+  "Judges" => "Jdg",
+  "Jdg" => "Jdg",
+  "Ruth" => "Rut",
+  "Rut" => "Rut",
+  "1 Samuel" => "1Sa",
+  "1Sa" => "1Sa",
+  "2 Samuel" => "2Sa",
+  "2Sa" => "2Sa",
+  "1 Kings" => "1Ki",
+  "1Ki" => "1Ki",
+  "2 Kings" => "2Ki",
+  "2Ki" => "2Ki",
+  "1 Chronicles" => "1Ch",
+  "1Ch" => "1Ch",
+  "2 Chronicles" => "2Ch",
+  "2Ch" => "2Ch",
+  "Ezra" => "Ezr",
+  "Ezr" => "Ezr",
+  "Nehemiah" => "Neh",
+  "Neh" => "Neh",
+  "Esther" => "Est",
+  "Est" => "Est",
+  "Job" => "Job",
+  "Psalm" => "Psa",
+  "Psa" => "Psa",
+  "Psalms" => "Psa",
+  "Proverbs" => "Pro",
+  "Pro" => "Pro",
+  "Ecclesiastes" => "Ecc",
+  "Ecclesiasties" => "Ecc",
+  "Ecc" => "Ecc",
+  "Song of Songs" => "SoS",
+  "Song of Solomon" => "SoS",
+  "SoS" => "SoS",
+  "Isaiah" => "Isa",
+  "Isa" => "Isa",
+  "Jeremiah" => "Jer",
+  "Jer" => "Jer",
+  "Lamentations" => "Lam",
+  "Lam" => "Lam",
+  "Ezekiel" => "Eze",
+  "Eze" => "Eze",
+  "Daniel" => "Dan",
+  "Dan" => "Dan",
+  "Hosea" => "Hos",
+  "Hos" => "Hos",
+  "Joel" => "Joe",
+  "Joe" => "Joe",
+  "Amos" => "Amo",
+  "Amo" => "Amo",
+  "Obadiah" => "Oba",
+  "Oba" => "Oba",
+  "Jonah" => "Jon",
+  "Jon" => "Jon",
+  "Micah" => "Mic",
+  "Mic" => "Mic",
+  "Nahum" => "Nah",
+  "Nah" => "Nah",
+  "Habakkuk" => "Hab",
+  "Hab" => "Hab",
+  "Zephaniah" => "Zep",
+  "Zep" => "Zep",
+  "Haggai" => "Hag",
+  "Hag" => "Hag",
+  "Zechariah" => "Zec",
+  "Zec" => "Zec",
+  "Malachi" => "Mal",
+  "Mal" => "Mal",
+  "Matthew" => "Mat",
+  "Mat" => "Mat",
+  "Mark" => "Mar",
+  "Mar" => "Mar",
+  "Luke" => "Luk",
+  "Luk" => "Luk",
+  "John" => "Joh",
+  "Joh" => "Joh",
+  "Acts" => "Act",
+  "Act" => "Act",
+  "Romans" => "Rom",
+  "Rom" => "Rom",
+  "1 Corinthians" => "1Co",
+  "1Co" => "1Co",
+  "2 Corinthians" => "2Co",
+  "2Co" => "2Co",
+  "Galatians" => "Gal",
+  "Gal" => "Gal",
+  "Ephesians" => "Eph",
+  "Eph" => "Eph",
+  "Philippians" => "Php",
+  "Php" => "Php",
+  "Colossians" => "Col",
+  "Col" => "Col",
+  "1 Timothy" => "1Ti",
+  "1Ti" => "1Ti",
+  "2 Timothy" => "2Ti",
+  "2Ti" => "2Ti",
+  "Titus" => "Tit",
+  "Tit" => "Tit",
+  "Philemon" => "Phm",
+  "Phm" => "Phm",
+  "Hebrews" => "Heb",
+  "Heb" => "Heb",
+  "James" => "Jam",
+  "Jam" => "Jam",
+  "1 Peter" => "1Pe",
+  "1Pe" => "1Pe",
+  "2 Peter" => "2Pe",
+  "2Pe" => "2Pe",
+  "1 John" => "1Jn",
+  "1Jn" => "1Jn",
+  "2 John" => "2Jn",
+  "2Jn" => "2Jn",
+  "3 John" => "3Jn",
+  "3Jn" => "3Jn",
+  "Jude" => "Jud",
+  "Jud" => "Jud",
+  "Revelation" => "Rev",
+  "Rev" => "Rev",
+  "Revleation" => "Rev",
+  "1 Thessalonians" => "1Th",
+  "2 Thessalonians" => "2Th",
+}
+
+def references_from_pericope(pericope)
+  recent_chapter = nil # e.g. in 12:1-8, remember that 12 is the chapter when we parse the 8
+  recent_chapter = 1 unless pericope.book_has_chapters?
+  pericope.ranges.map do |range|
+    min_chapter = Pericope.get_chapter(range.begin)
+    min_verse = Pericope.get_verse(range.begin)
+    max_chapter = Pericope.get_chapter(range.end)
+    max_verse = Pericope.get_verse(range.end)
+    s = ""
+
+    if min_verse == 1 and max_verse >= Pericope.get_max_verse(pericope.book, max_chapter)
+      s << min_chapter.to_s
+      s << "-#{max_chapter}" if max_chapter > min_chapter
+    else
+      if false && recent_chapter == min_chapter
+        s << min_verse.to_s
+      else
+        recent_chapter = min_chapter
+        s << "#{min_chapter}:#{min_verse}"
+      end
+
+      if range.count > 1
+
+        s << "-"
+        if min_chapter == max_chapter
+          s << max_verse.to_s
+        else
+          recent_chapter = max_chapter
+          s << "#{max_chapter}:#{max_verse}"
+        end
+      end
+    end
+
+    {
+      "book" => pericope.book_name,
+      # "reference" => s,
+      "minChapter" => min_chapter,
+      "minVerse" => min_verse,
+      "maxChapter" => max_chapter,
+      "maxVerse" => max_verse
+    }
+  end
+end
+
+json = {}
+
+["Foundational", "Family", "Economics", "Religion", "Government", "Education", "Communication", "Celebration"].each do |sphere_name|
+  sphere = {}
+  filename = File.expand_path("../../SVB2/Spheres/KeyPassages/#{sphere_name}-Table 1.csv")
+  rows = CSV.open(filename, headers: true).read
+  sectionKey = nil
+  section = nil
+  rows.each do |row|
+    next if row["index"].nil?
+    if row["index"].to_i == 0
+      sectionKey = row["index"]
+      section = []
+      sphere[sectionKey] = section
+    else
+      if pericope = Pericope.parse_one(row["reference"])
+        references = references_from_pericope(pericope).map do |reference|
+          # EMDROS[:source_objects].each do |source_object|
+          if djhref = BOOK_TO_DJHREF[pericope.book_name]
+            sql = if reference["minChapter"] == reference["maxChapter"]
+              "SELECT * FROM verse_objects WHERE (mdf_djhbook = '#{djhref}' AND mdf_chapter = #{reference["minChapter"]} AND mdf_verse_start >= #{reference["minVerse"]} AND mdf_verse_end <= #{reference["maxVerse"]});"
+            else
+              "SELECT * FROM verse_objects WHERE (mdf_djhbook = '#{djhref}' AND mdf_chapter = #{reference["minChapter"]} AND mdf_verse_start >= #{reference["minVerse"]}) OR (mdf_djhbook = '#{djhref}' AND mdf_chapter = #{reference["maxChapter"]} AND mdf_verse_end <= #{reference["maxVerse"]});"
+            end
+            STDOUT.puts sql
+          else
+            STDERR.puts "Could not find reference for #{pericope.book_name}"
+          end
+          reference
+        end
+
+        section << {
+          "number" => row["index"].to_i,
+          "title" => row["title"],
+          "subtitle" => pericope.to_s,
+          "references" => references
+        }
+      else
+        STDERR.puts "Could not parse reference #{row["reference"]}"
+      end
+    end
+  end
+  json[sphere_name] = sphere
+end
+
+# STDOUT.puts JSON.pretty_generate(json)
