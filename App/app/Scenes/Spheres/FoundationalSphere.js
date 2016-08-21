e43bc03eb196a544ff9806ed4536ac3d694ea655
Jonathan Younger <jonathan@daikini.com>
2016-08-21 11:55:10 -0600

    Spheres WIP

--- App/app/Scenes/Spheres/FoundationalSphere.js
+++ App/app/Scenes/Spheres/FoundationalSphere.js
@@ -114,7 +114,6 @@ export default class FoundationalSphere extends Component {
           <Text style={styles.readButtonTitle}>Explore 52 key passages</Text>
         </TouchableOpacity>
         <View style={styles.contentContainer}>
-          <Text style={[styles.contentBody, {marginBottom: -25, marginTop: -10}]}>Introduction</Text>
           <Text style={[styles.contentBody, {marginTop: 5}]}>{sphere.description}</Text>
         </View>
         <SegmentedControl
