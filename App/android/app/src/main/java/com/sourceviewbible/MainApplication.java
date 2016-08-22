e95d258b5de8300fdfcb872c6f0ae0c6e7c5c2f7
Jonathan Younger <jonathan@daikini.com>
2016-08-22 08:45:56 -0600

    Fabric

--- App/android/app/src/main/java/com/sourceviewbible/MainApplication.java
+++ App/android/app/src/main/java/com/sourceviewbible/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.smixx.fabric.FabricPackage;
 import com.rnfs.RNFSPackage;
 import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FabricPackage(),
           new RNFSPackage(),
           new RealmReactPackage(),
           new VectorIconsPackage(),
