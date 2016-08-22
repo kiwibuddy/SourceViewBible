32578704fd03f34ddef44b6f7757c36f13cec164
Jonathan Younger <jonathan@daikini.com>
2016-08-22 12:38:11 -0600

    Sphere Source Types

--- App/app/Scenes/Discover/DiscoverSpheres.js
+++ App/app/Scenes/Discover/DiscoverSpheres.js
@@ -142,7 +142,7 @@ export default class DiscoverSpheres extends Component {
                   style={{flex: 0, marginLeft: 4}}
                   barStyle={{width: 2, height: 12, marginHorizontal: 1}}
                   horizontal={false}
-                  data={[{narrator: 1}, {god: 1}, {lead: 1}, {support: 1}]}
+                  data={[{narrator: sphere.countOfSourceType(SourceType.NARRATOR)}, {god: sphere.countOfSourceType(SourceType.GOD)}, {lead: sphere.countOfSourceType(SourceType.LEAD)}, {support: sphere.countOfSourceType(SourceType.SUPPORT)}]}
                 />
               </View>
             </View>
