6eecc45529a95d2d044c1ce71b6146694a91f4e9
Jonathan Younger <jonathan@daikini.com>
2016-08-24 10:16:10 -0600

    PieSpike WIP

--- TechnicalValidation/PieSpike/PieChart.js
+++ TechnicalValidation/PieSpike/PieChart.js
@@ -1,41 +1,25 @@
 /* @flow */
 'use strict';
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  TouchableOpacity,
+  View,
 } from 'react-native';
+import ColorPropType from 'ColorPropType';
 
 import Svg,{
     Circle,
-    Ellipse,
-    G,
-    LinearGradient,
-    RadialGradient,
-    Line,
-    Path,
-    Polygon,
-    Polyline,
-    Rect,
-    Symbol,
-    Use,
-    Defs,
-    Stop
 } from 'react-native-svg';
 
+
 const PieChart = (props: Object) => {
-  const circleRadius = 25;
-
-  /* 2π × 25 */
-  const radius = 2 * Math.PI * circleRadius;
-
   const strokeWidth = 4;
-
   return (
     <View style={styles.chart}>
-      <Svg width="200" height="200" viewBox="0 0 34 32" style={{margin: 10, backgroundColor: 'purple'}}>
+      <Svg width="200" height="200" viewBox="0 0 34 32">
         <Circle
           r="16" cx="17" cy="16"
           fill="transparent"
@@ -71,7 +55,6 @@ const PieChart = (props: Object) => {
 
 const styles = StyleSheet.create({
   chart: {
-    backgroundColor: 'orange',
     transform: [{rotate: '-90deg'}],
   }
 })
