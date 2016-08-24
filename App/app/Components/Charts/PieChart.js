efdce09c56d6cb66c3101eea75dab4f144b3ef52
Jonathan Younger <jonathan@daikini.com>
2016-08-24 11:11:36 -0600

    Refactor Pie Chart WIP

--- App/app/Components/Charts/PieChart.js
+++ App/app/Components/Charts/PieChart.js
@@ -23,46 +23,42 @@ const PieChart = (props: Object) => {
   const { color, size, slices: data, titleStyle, subtitleStyle } = props;
   const chartStyle = [styles.chart, props.style, {width: size, height: size}];
   const strokeWidth = 4;
+
+  const title = props.title ? <Text numberOfLines={1} style={[styles.title, {color: color}, titleStyle]}>{props.title}</Text> : null;
+  const subtitle = props.subtitle ? <Text numberOfLines={1} style={[styles.subtitle, subtitleStyle]}>{props.subtitle}</Text> : null;
+
+
+  const slices = data.filter(slice => slice.value > 0);
+  const sum = slices.reduce((sum, slice) => sum += slice.value, 0);
+
+  let runningTotal = 0;
+  const pieSlices = slices.map((slice, index) => {
+    const slicePercent = (slice.value / sum) * 100;
+
+    const pieSlice = <Circle
+      key={'slice-' + index}
+      r="16" cx="17" cy="16"
+      fill="transparent"
+      stroke={slice.color}
+      strokeWidth={strokeWidth/2}
+      strokeDasharray={[slicePercent, 100]}
+      strokeDashoffset={-runningTotal}
+    />;
+
+    runningTotal += slicePercent;
+    return pieSlice;
+  });
+
   return (
     <TouchableOpacity style={chartStyle} onPress={props.onPress}>
-      <View style={styles.pie}>
+      <View style={[styles.pie, StyleSheet.absoluteFill]}>
         <Svg width={size} height={size} viewBox="0 0 34 32">
-          <Circle
-            r="16" cx="17" cy="16"
-            fill="transparent"
-            stroke="gray"
-            strokeWidth={strokeWidth/2}
-            strokeDasharray={[25, 100]}
-            strokeDashoffset={-75}
-          />
-          <Circle
-            r="16" cx="17" cy="16"
-            fill="transparent"
-            stroke="blue"
-            strokeWidth={strokeWidth/2}
-            strokeDasharray={[25, 100]}
-            strokeDashoffset={-50}
-          />
-          <Circle
-            r="16" cx="17" cy="16"
-            fill="transparent"
-            stroke="green"
-            strokeWidth={strokeWidth/2}
-            strokeDasharray={[25, 100]}
-            strokeDashoffset={-25}
-          />
-          <Circle
-            r="16" cx="17" cy="16"
-            fill="transparent"
-            stroke="red"
-            strokeWidth={strokeWidth/2}
-            strokeDasharray={[25, 100]}
-            strokeDashoffset={0}
-          />
+          {pieSlices}
         </Svg>
       </View>
+      {title}
+      {subtitle}
     </TouchableOpacity>
-
   );
 }
 
