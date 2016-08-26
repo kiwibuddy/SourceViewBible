e3d3a4570a6bb8d7ae6f56ef63cbc8a7345bd01c
Harold <harold@overcommittedapps.com>
2016-08-26 13:25:48 -0700

    Sphere help content

--- App/app/Scenes/Spheres/Help.js
+++ App/app/Scenes/Spheres/Help.js
@@ -38,8 +38,12 @@ const Help = (props: Props) => {
       />
       <ScrollView style={styles.scrollView}>
         <View style={styles.helpContainer}>
-          <Text style={styles.contentHeader}>Spheres Overview</Text>
-          <Text style={styles.contentBody}>...</Text>
+          <Text style={styles.contentBody}>Society is shaped by seven spheres of influence. With the SphereView in-app purchase unlocked, you'll be able to make personal observations about how scripture can be used to shape a Biblical Christian worldview. Your purchase of the SphereView dataset helps us continue our work to provide innovative Biblical discovery tools and is greatly appreciated.</Text>
+          <View style={styles.separator} />
+          <Text style={styles.contentH2}>Sphere Overview Screens</Text>
+          <Text style={styles.contentBody}>Tap on any sphere to get an overview of how that sphere is represented in Scripture. See which books the sphere appears, how many sources have words highlighted in that sphere, and how many words in total across all sources. Each sphere includes 52 key passages that reflect the essence of the sphere. And as you scroll down on the sphere overview screen, you can learn more about how we approached noting spheres and receive some guidance about how to read through the Bible with spheres highlighted. You will also see the top books and sources for the sphere by word count.</Text>
+          <Text style={styles.contentH2}>What can I do with Spheres?</Text>
+          <Text style={styles.contentBody}>You can read Scripture with SphereView highlights, explore key passages, and meditate on how a source's words relate to societal spheres. It's an exciting new way to discover more about what God has to say.</Text>
         </View>
         <HelpFooter navigate={props.navigate} />
       </ScrollView>
