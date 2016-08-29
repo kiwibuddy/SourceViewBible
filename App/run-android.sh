cd62b486ce4791d11bb76cc6cb130ff0ac9d8178
Jonathan Younger <jonathan@daikini.com>
2016-08-29 06:58:42 -0600

    Fix android run script

--- App/run-android.sh
+++ App/run-android.sh
@@ -3,4 +3,9 @@
 adb shell "pm uninstall com.sourceviewbible"
 adb shell "rm -rf /data/app/com.sourceviewbible-*"
 
-react-native run-android --variant ${1:debug}
+if [ -z "$1" ]
+  then
+    react-native run-android
+else
+  react-native run-android --variant release
+fi
