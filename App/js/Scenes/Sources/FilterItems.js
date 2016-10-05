cb5562c79bc4f46369c70ba96853cee96a5699b8
Jonathan Younger <jonathan@daikini.com>
2016-10-05 11:56:10 -0600

    Filters WIP

--- App/js/Scenes/Sources/FilterItems.js
+++ App/js/Scenes/Sources/FilterItems.js
@@ -3,31 +3,62 @@
 
 import React, { Component } from 'react';
 import {
-  Image,
-  ListView,
+  ScrollView,
   Text,
-  View
+  TouchableOpacity,
+  View,
+  Image,
 } from 'react-native';
 
 import {
-  Analytics,
   Colors,
+  Localizable,
   StyleSheet,
-  Localizable
 } from '../../Common';
 
-import { sourceURL, sourcesFilterURL } from '../../Navigation';
+import { genderFilterURL, natureFilterURL, professionFilterURL, roleFilterURL } from '../../Navigation';
 
 type Props = {
-
+  navigate: Function,
+  onDone: Function,
 };
 
 const FilterItems = (props: Props) => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => props.navigate(roleFilterURL({title: 'Role'}))}>
+        <Text style={StyleSheet.styles.cell.title}>Role</Text>
+        <Image source={require('../../Images/common/disclosure.png')} />
+      </TouchableOpacity>
+      <View style={styles.separator} />
+      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => props.navigate(natureFilterURL({title: 'Nature'}))}>
+        <Text style={StyleSheet.styles.cell.title}>Nature</Text>
+        <Image source={require('../../Images/common/disclosure.png')} />
+      </TouchableOpacity>
+      <View style={styles.separator} />
+      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => props.navigate(genderFilterURL({title: 'Gender'}))}>
+        <Text style={StyleSheet.styles.cell.title}>Gender</Text>
+        <Image source={require('../../Images/common/disclosure.png')} />
+      </TouchableOpacity>
+      <View style={styles.separator} />
+      <TouchableOpacity style={StyleSheet.styles.listItem} onPress={() => props.navigate(professionFilterURL({title: 'Profession'}))}>
+        <Text style={StyleSheet.styles.cell.title}>Profession</Text>
+        <Image source={require('../../Images/common/disclosure.png')} />
+      </TouchableOpacity>
+      <View style={styles.separator} />
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  separator: {
+    ...StyleSheet.styles.separator,
+    marginLeft: 15,
+  },
+});
 
 export default FilterItems;
