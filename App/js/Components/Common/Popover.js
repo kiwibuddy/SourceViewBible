55d0a9bfc3ccaa309639872dff9e0b8f9e14e007
Jonathan Younger <jonathan@daikini.com>
2016-10-05 10:14:18 -0600

    Modal

--- App/js/Components/Common/Popover.js
+++ App/js/Components/Common/Popover.js
@@ -4,74 +4,11 @@
 import React, { Component, PropTypes } from 'react';
 
 import {
-  Image,
-  Navigator,
   Platform,
-  Text,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 
-import EventEmitter from 'EventEmitter';
-
-const NavigatorNavigationBarStyles = {
-  ...Navigator.NavigationBar.Styles,
-  General: {
-    ...Navigator.NavigationBar.Styles.General,
-    TotalNavHeight: Navigator.NavigationBar.Styles.General.NavBarHeight
-  },
-  Stages: {
-    ...Navigator.NavigationBar.Styles.Stages,
-    Left: {
-      ...Navigator.NavigationBar.Styles.Stages.Left,
-      Title: {
-        ...Navigator.NavigationBar.Styles.Stages.Left.Title,
-        top: 0,
-      },
-      LeftButton: {
-        ...Navigator.NavigationBar.Styles.Stages.Left.LeftButton,
-        top: 0,
-      },
-      RightButton: {
-        ...Navigator.NavigationBar.Styles.Stages.Left.RightButton,
-        top: 0,
-      }
-    },
-    Center: {
-      ...Navigator.NavigationBar.Styles.Stages.Center,
-      Title: {
-        ...Navigator.NavigationBar.Styles.Stages.Center.Title,
-        top: 0,
-      },
-      LeftButton: {
-        ...Navigator.NavigationBar.Styles.Stages.Center.LeftButton,
-        top: 0,
-      },
-      RightButton: {
-        ...Navigator.NavigationBar.Styles.Stages.Center.RightButton,
-        top: 0,
-      }
-    },
-    Right: {
-      ...Navigator.NavigationBar.Styles.Stages.Right,
-      Title: {
-        ...Navigator.NavigationBar.Styles.Stages.Right.Title,
-        top: 0,
-      },
-      LeftButton: {
-        ...Navigator.NavigationBar.Styles.Stages.Right.LeftButton,
-        top: 0,
-      },
-      RightButton: {
-        ...Navigator.NavigationBar.Styles.Stages.Right.RightButton,
-        top: 0,
-      }
-    }
-  }
-};
-
-const NAV_BAR_HEIGHT = NavigatorNavigationBarStyles.General.NavBarHeight;
+import Modal from './Modal';
 
 import {
   Colors,
@@ -79,154 +16,29 @@ import {
   StyleSheet,
 } from '../../Common';
 
-import router, { BACK } from '../../Navigation';
-
 type Props = {
   initialRoute: Object,
+  popoverStyle: any,
   onPressCancel: Function,
   onDone: Function,
 };
 
 export default class Popover extends Component {
   props: Props;
-  eventEmitter: Object;
-
-  componentWillMount() {
-    this.eventEmitter = new EventEmitter();
-  }
 
   render() {
     return (
       <View style={styles.overlayContainer}>
-        <Navigator
+        <Modal
+          {...this.props}
           initialRoute={this.props.initialRoute}
-          navigationBar={this._renderNavigationBar()}
-          renderScene={this._renderScene}
-          style={styles.popover}
+          modalStyle={styles.popover}
+          onPressCancel={this.props.onPressCancel}
+          onPressDone={this.props.onPressDone}
         />
       </View>
     );
   }
-
-  _renderNavigationBar = () => {
-    const {onPressCancel} = this.props;
-    const eventEmitter = this.eventEmitter;
-
-    return (
-      <Navigator.NavigationBar
-        navigationStyles={NavigatorNavigationBarStyles}
-        routeMapper={{
-          LeftButton: function(route, navigator, index, navState) {
-            if (index === 0) {
-              if (Platform.OS === 'android') {
-                return (
-                  <TouchableOpacity
-                    onPress={() => onPressCancel()}
-                    style={styles.navBarLeftButton}
-                    >
-                    <Image source={require('../../Components/Navigation/Images/nav-close.png')} style={{width: 24, height: 24, tintColor: Colors.tint}} />
-                  </TouchableOpacity>
-                );
-              }
-
-              return (
-                <TouchableOpacity
-                  onPress={() => onPressCancel()}
-                  style={styles.navBarLeftButton}>
-                  <Text style={[styles.navBarText, styles.navBarButtonText]}>
-                    {Localizable.t('cancel')}
-                  </Text>
-                </TouchableOpacity>
-              );
-            }
-
-            if (Platform.OS === 'android') {
-              return (
-                <TouchableOpacity
-                  onPress={() => navigator.pop()}
-                  style={styles.navBarLeftButton}
-                  >
-                  <Image source={require('../../Components/Navigation/Images/nav-back.png')} style={{width: 24, height: 24, tintColor: Colors.tint}} />
-                </TouchableOpacity>
-              );
-            }
-
-            const title = Localizable.t('back');
-            return (
-              <TouchableOpacity
-                onPress={() => navigator.pop()}
-                style={styles.navBarLeftButton}>
-                <Text style={[styles.navBarText, styles.navBarButtonText]}>
-                  {title}
-                </Text>
-              </TouchableOpacity>
-            );
-          },
-          RightButton: function(route, navigator, index, navState) {
-            if (route.path !== '/DiscoveryCenter/Filters/SpheresFilter') return null;
-
-            if (Platform.OS === 'android') {
-              return (
-                <TouchableOpacity
-                  onPress={() => eventEmitter.emit('onPressDone')}
-                  style={styles.navBarRightButton}>
-                  <Image source={require('../../Components/Navigation/Images/checkmark-icon.png')} style={{width: 24, height: 24, tintColor: Colors.tint}} />
-                </TouchableOpacity>
-              );
-            }
-
-            return (
-              <TouchableOpacity
-                onPress={() => eventEmitter.emit('onPressDone')}
-                style={styles.navBarRightButton}>
-                <Text style={[styles.navBarText, styles.navBarButtonText, StyleSheet.styles.navigationBar.doneButtonTitle]}>
-                  {Localizable.t('done')}
-                </Text>
-              </TouchableOpacity>
-            );
-          },
-          Title: function(route, navigator, index, navState) {
-            return (
-              <Text style={[styles.navBarText, styles.navBarTitleText]}>
-                {route.title}
-              </Text>
-            );
-          },
-        }}
-        style={styles.navBar}
-      />
-    );
-  };
-
-  _renderScene = (navigatorRoute: Object, navigator: Object) => {
-    const { route, params } = router.match(navigatorRoute.path);
-
-    if (!route) {
-      throw new Error('Could not find route for: ' + navigatorRoute.path);
-    }
-
-    const Scene = route.scene;
-    return (
-      <View style={{flex: 1, paddingTop: NAV_BAR_HEIGHT}}>
-        <Scene
-          {...this.props}
-          {...navigatorRoute}
-          {...params}
-          navigate={(route: any, options?: any) => this._navigate(navigator, route, options)}
-          onDone={this.props.onDone}
-          events={this.eventEmitter}
-        />
-      </View>
-    );
-  };
-
-  _navigate = (navigator: Object, route: any, options?: any) => {
-    if (route === BACK) {
-      navigator.pop();
-    } else {
-      navigator.push(route);
-    }
-  };
 }
 
 const styles = StyleSheet.create({
@@ -251,49 +63,5 @@ const styles = StyleSheet.create({
         top: 4,
       },
     })
-  },
-  navBar: {
-    height: NAV_BAR_HEIGHT,
-    backgroundColor: Platform.OS === 'ios' ? 'rgba(248, 248, 248, .85)' : '#FFF',
-    borderBottomColor: 'rgba(0, 0, 0, .15)',
-    borderBottomWidth: Platform.OS === 'ios' ? StyleSheet.hairlineWidth : 0,
-    elevation: 2,
-    marginBottom: 16, // This is needed for elevation shadow
-  },
-  navBarText: {
-    fontSize: 16,
-    marginVertical: 12,
-  },
-  navBarTitleText: {
-    color: 'black',
-    fontSize: 17,
-    fontWeight: 'bold',
-    marginVertical: 12,
-    ...Platform.select({
-      android: {
-        marginVertical: 16,
-      },
-    })
-  },
-  navBarLeftButton: {
-    paddingLeft: 10,
-    ...Platform.select({
-      android: {
-        paddingHorizontal: 10,
-        paddingVertical: 16,
-      },
-    })
-  },
-  navBarRightButton: {
-    paddingRight: 10,
-    ...Platform.select({
-      android: {
-        paddingHorizontal: 10,
-        paddingVertical: 16,
-      },
-    })
-  },
-  navBarButtonText: {
-    color: Colors.tint,
   },
 });
