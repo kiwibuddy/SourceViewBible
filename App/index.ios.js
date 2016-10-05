2f245f8bdb0052df9b84706dfc478d7a53582e5d
Jonathan Younger <jonathan@daikini.com>
2016-10-05 08:56:57 -0600

    Rename "app" folder to "js"

--- App/index.ios.js
+++ App/index.ios.js
@@ -1,6 +1,6 @@
 'use strict';
 
 import { AppRegistry } from 'react-native';
-import SourceViewBible from './app/SourceViewBible';
+import SourceViewBible from './js/SourceViewBible';
 
 AppRegistry.registerComponent('SourceViewBible', () => SourceViewBible);
