c0a47fbad4568d0cfcd006e48a285e7f2841cb57
Jonathan Younger <jonathan@daikini.com>
2016-10-13 09:40:18 -0600

    IAP WIP

--- App/js/API/Store.android.js
+++ App/js/API/Store.android.js
@@ -6,40 +6,40 @@ import InAppBilling from 'react-native-billing';
 export default class Store {
   static products(productIdentifiers) {
     return new Promise((resolve, reject) => {
-      InAppBilling.open()
-      .then(() => InAppBilling.getProductDetailsArray(productIdentifiers))
-      .then((products) => {
-        InAppBilling.close();
-        resolve(products.map((product) => {
-          return {
-            productID: product.productId,
-            localizedPrice: product.priceText,
-          };
-        }));
+      InAppBilling.open().then(() => {
+        InAppBilling.getProductDetailsArray(productIdentifiers).then((products) => {
+          InAppBilling.close();
+
+          resolve(products.map((product) => {
+            return {
+              productID: product.productId,
+              localizedPrice: product.priceText,
+            };
+          }));
+        });
       });
     });
   }
 
   static purchase(productID) {
     return new Promise((resolve, reject) => {
-      InAppBilling.open()
-      .then(() => InAppBilling.purchase('android.test.purchased'))
-      .then((transationDetail) => {
-        console.log(transationDetail);
-
-        InAppBilling.close();
-        resolve(transationDetail.purchaseState === 'PurchasedSuccessfully');
+      InAppBilling.open().then(() => {
+        InAppBilling.purchase('android.test.purchased').then((transationDetail) => {
+          InAppBilling.close();
+          resolve(transationDetail.purchaseState === 'PurchasedSuccessfully');
+        });
       });
     });
   }
 
   static restorePurchases() {
     return new Promise((resolve, reject) => {
-      InAppBilling.open()
-      .then(() => InAppBilling.listOwnedProducts())
-      .then((productIdentifiers) => {
-        InAppBilling.close();
-        resolve(productIdentifiers);
+      InAppBilling.open().then(() => {
+        InAppBilling.listOwnedProducts().then((productIdentifiers) => {
+          console.log('productIdentifiers', productIdentifiers);
+          InAppBilling.close();
+          resolve(productIdentifiers);
+        });
       });
     });
   }
