1cb3f07b520ec1705a4267a935c59af18f737846
Jonathan Younger <jonathan@daikini.com>
2016-10-27 09:56:27 -0600

    Fix android breakage

--- App/android/app/src/main/java/com/sourceviewbible/MainApplication.java
+++ App/android/app/src/main/java/com/sourceviewbible/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.idehub.Billing.InAppBillingBridgePackage;
 import com.horcrux.svg.RNSvgPackage;
@@ -47,8 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
 
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNViewShotPackage(),
-            new RNDeviceInfo(),
+          new RNDeviceInfo(),
           new InAppBillingBridgePackage(licenseKey),
           new RNSvgPackage(),
           new RNAppInfoPackage(),
