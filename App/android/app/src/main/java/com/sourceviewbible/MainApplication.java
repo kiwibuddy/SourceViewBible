6c9c5a00b88a77506bbe2baa2fb812b021871b3a
Jonathan Younger <jonathan@daikini.com>
2016-10-27 10:30:19 -0600

    Fix android breakage

--- App/android/app/src/main/java/com/sourceviewbible/MainApplication.java
+++ App/android/app/src/main/java/com/sourceviewbible/MainApplication.java
@@ -46,13 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
 
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-<<<<<<< HEAD
-            new WebViewBridgePackage(),
-            new RNViewShotPackage(),
-            new RNDeviceInfo(),
-=======
           new RNDeviceInfo(),
->>>>>>> release-1.1
           new InAppBillingBridgePackage(licenseKey),
           new RNSvgPackage(),
           new RNAppInfoPackage(),
