9c33d7d10f051c416abdfada7111d1b145417b26
Jonathan Younger <jonathan@daikini.com>
2018-07-07 08:36:52 -0600

    Upgrade RN

--- App/index.android.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import {AppRegistry} from 'react-native';
-import SourceViewBible from './js/SourceViewBible';
-
-AppRegistry.registerComponent('SourceViewBible', () => SourceViewBible);
